45f05c6aaabe8c7a25f6b111a282567aa9b942e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 18:31:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = ' <a href="#" class="support-contact"></a>';
+$lang['support_app_name'] = 'Support';
+$lang['support_chat_not_available'] = 'Support via chat is unavailable at this time.';
+$lang['support_community_forums'] = 'Community Forums';
+$lang['support_contact_upgrades'] = '<a href="#" class="support-contact"></a>';
+$lang['support_day_remaining'] = 'Day Remaining';
+$lang['support_days_remaining'] = 'Days Remaining';
+$lang['support_documentation'] = 'Documentation';
+$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
+$lang['support_learn_more'] = 'Learn more';
+$lang['support_no_open_tickets'] = 'No Open Tickets';
+$lang['support_not_available'] = 'Not Available';
+$lang['support_phone'] = 'Phone';
+$lang['support_please_open_ticket'] = 'Please open a support ticket before initiating a chat session.';
+$lang['support_realtime_chat'] = 'Real Time Chat Support';
+$lang['support_submit_bug_report'] = 'Submit a Bug Report';
+$lang['support_submit_ticket'] = 'Submit a Support Ticket';
+$lang['support_upgrades_and_per_incident'] = '<a href="#" class="support-upgrade-url"></a>';
+$lang['support_upgrade'] = 'Upgrade';
+$lang['support_upgrade_window'] = 'Upgrade Eligibility';
+$lang['support_web'] = 'Web';
