c3cdbaac3ca96de0a3dc45a98f0922a3d2b0320a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:24:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Sizning tizimini qo&#39;llab-quvvatlash uchun imkoniyatlari va resurslari. Agar siz talablariga iltimos muhokama zarur bo&#39;lsa, <a href="#" class="support-contact"> Biz bilan bog&#39;lanish </a> .';
+$lang['support_app_name'] = 'Support';
+$lang['support_chat_not_available'] = 'chat orqali qo&#39;llab-quvvatlash, bu vaqtda mavjud emas.';
+$lang['support_community_forums'] = 'Omma Forumlar';
+$lang['support_contact_upgrades'] = 'biz bilan aloqa <a href="#" class="support-contact"> Oshirish jamoasi </a> variantlari uchun.';
+$lang['support_day_remaining'] = 'Qolgan kun';
+$lang['support_days_remaining'] = 'qolgan kunlar';
+$lang['support_documentation'] = 'hujjatlar';
+$lang['support_knowledge_base'] = 'Advanced Knowledge base';
+$lang['support_learn_more'] = 'Ko&#39;proq ma&#39;lumot olish';
+$lang['support_no_open_tickets'] = 'No Open chipta';
+$lang['support_not_available'] = 'Mavjud emas';
+$lang['support_phone'] = 'Telefon';
+$lang['support_please_open_ticket'] = 'a chat sessiyasi boshlash oldin qo&#39;llab-quvvatlash yo&#39;llanma oching.';
+$lang['support_realtime_chat'] = 'Real Time Chat qo&#39;llab-quvvatlash';
+$lang['support_submit_bug_report'] = 'a xato haqida xabar qiling';
+$lang['support_submit_ticket'] = 'a Support Ticket Submit';
+$lang['support_upgrades_and_per_incident'] = 'qo&#39;llab-quvvatlash uchun loyiq bo&#39;lish uchun, har bir-hodisa chipta sotib yoki obuna darajasini yuksaltirish iltimos <a href="#" class="support-upgrade-url"> bu yerda </a> .';
+$lang['support_upgrade'] = 'Upgrade';
+$lang['support_upgrade_window'] = 'Tanlov huquqi oshirish';
+$lang['support_web'] = 'Web';
