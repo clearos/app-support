e3e68de913d5e7983860d68d0d99f55961ba9692
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:44:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Опције и средства за подршку систем. Ако желите да разговарамо о вашим захтевима, молим <a href="#" class="support-contact"> Контактирајте нас </a> .';
+$lang['support_app_name'] = 'Подршка';
+$lang['support_chat_not_available'] = 'Подршка преко цхат није доступна у овом тренутку.';
+$lang['support_community_forums'] = 'Форум';
+$lang['support_contact_upgrades'] = 'Контактирајте наш <a href="#" class="support-contact"> надоградња тим </a> за опције.';
+$lang['support_day_remaining'] = 'dan Преостали';
+$lang['support_days_remaining'] = 'Преостали дана';
+$lang['support_documentation'] = 'документација';
+$lang['support_knowledge_base'] = 'Напредна База знања';
+$lang['support_learn_more'] = 'Сазнајте више';
+$lang['support_no_open_tickets'] = 'Нема Опен Тицкетс';
+$lang['support_not_available'] = 'Није доступно';
+$lang['support_phone'] = 'телефон';
+$lang['support_please_open_ticket'] = 'Молимо Вас да отворите суппорт тицкет пре покретања ћаскање.';
+$lang['support_realtime_chat'] = 'Реалном времену Чат Подршка';
+$lang['support_submit_bug_report'] = 'Поднесе извештај о грешкама';
+$lang['support_submit_ticket'] = 'Доставити Суппорт Тицкет';
+$lang['support_upgrades_and_per_incident'] = 'Да би испунили услове за подршку, купите карту по-инцидента или надоградите ниво претплате <a href="#" class="support-upgrade-url"> овде </a> .';
+$lang['support_upgrade'] = 'упграде';
+$lang['support_upgrade_window'] = 'Упграде право наступа';
+$lang['support_web'] = 'веб';
