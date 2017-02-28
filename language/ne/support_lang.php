db61d0b1c69153cedb0c390ecad8e03b632cfa53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:56:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'तपाईंको सिस्टम समर्थन विकल्प र स्रोतहरू। तपाईं आवश्यकताहरु आफ्नो छलफल गर्न कृपया चाहनुहुन्छ भने <a href="#" class="support-contact"> हामीलाई सम्पर्क गर्नुहोस </a> ।';
+$lang['support_app_name'] = 'समर्थन';
+$lang['support_chat_not_available'] = 'च्याट मार्फत समर्थन यस समय उपलब्ध छैन।';
+$lang['support_community_forums'] = 'सामुदायिक फोरम';
+$lang['support_contact_upgrades'] = 'सम्पर्क हाम्रो <a href="#" class="support-contact"> उन्नयन टोली </a> विकल्पहरू लागि।';
+$lang['support_day_remaining'] = 'दिन बाँकी';
+$lang['support_days_remaining'] = 'बाँकी दिन';
+$lang['support_documentation'] = 'दस्तावेज';
+$lang['support_knowledge_base'] = 'विकसित नलेज बेस';
+$lang['support_learn_more'] = 'अझै सिक';
+$lang['support_no_open_tickets'] = 'कुनै टिकट खुला';
+$lang['support_not_available'] = 'उपलब्ध छैन';
+$lang['support_phone'] = 'फोन';
+$lang['support_please_open_ticket'] = 'कृपया च्याट सत्र अघि एक समर्थन टिकट खोल्न।';
+$lang['support_realtime_chat'] = 'रियल समय चैट समर्थन';
+$lang['support_submit_bug_report'] = 'एक बगप्रतिवेदनपेशगर्नुहोस्';
+$lang['support_submit_ticket'] = 'समर्थन टिकट पेश';
+$lang['support_upgrades_and_per_incident'] = 'सहयोगको लागि योग्य हुन, कृपया प्रति-घटना एउटा टिकट खरिद वा आफ्नो सदस्यता स्तर स्तरवृद्धि <a href="#" class="support-upgrade-url"> यहाँ </a> ।';
+$lang['support_upgrade'] = 'अपग्रेड गर्नुहोस्';
+$lang['support_upgrade_window'] = 'योग्यता अपग्रेड';
+$lang['support_web'] = 'वेब';
