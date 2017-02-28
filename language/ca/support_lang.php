36d926585aa25a056cf605e62bf95672cdbb4dc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 03:10:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Opcions i recursos per donar suport el seu sistema. Si desitja parlar sobre les seves necessitats, si us plau <a href="#" class="support-contact"> contacti&#39;ns </a> .';
+$lang['support_app_name'] = 'suport';
+$lang['support_chat_not_available'] = 'El suport a través de xat no està disponible en aquest moment.';
+$lang['support_community_forums'] = 'Fòrums de la comunitat';
+$lang['support_contact_upgrades'] = 'Poseu-vos en contacte amb la nostra <a href="#" class="support-contact"> equip d&#39;actualització </a> per a les opcions.';
+$lang['support_day_remaining'] = 'dia restant';
+$lang['support_days_remaining'] = 'Dies restants';
+$lang['support_documentation'] = 'documentació';
+$lang['support_knowledge_base'] = 'Avançada Knowledge Base';
+$lang['support_learn_more'] = 'Aprèn més';
+$lang['support_no_open_tickets'] = 'Cap entrada Obertes';
+$lang['support_not_available'] = 'no disponible';
+$lang['support_phone'] = 'telèfon';
+$lang['support_please_open_ticket'] = 'Si us plau, obriu un tiquet de suport abans d&#39;iniciar una sessió de xat.';
+$lang['support_realtime_chat'] = 'Temps real Support Xat';
+$lang['support_submit_bug_report'] = 'Presentar un informe d&#39;error';
+$lang['support_submit_ticket'] = 'Presentar un tiquet de suport';
+$lang['support_upgrades_and_per_incident'] = 'Per ser elegible per a l&#39;ajuda, si us plau, comprar un bitllet per incident o actualitzar el seu nivell de subscripció <a href="#" class="support-upgrade-url"> aquí </a> .';
+$lang['support_upgrade'] = 'modernització';
+$lang['support_upgrade_window'] = 'Possibilitat d&#39;actualització';
+$lang['support_web'] = 'web';
