52e1ecc4f89c2c7fb565463c62fcd425dcbbf6de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:17:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'ಆಯ್ಕೆಗಳು ಮತ್ತು ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯ ಪೋಷಕ ಮೂಲಗಳು. ನೀವು ದಯವಿಟ್ಟು, ನಿಮ್ಮ ಅವಶ್ಯಕತೆಗಳನ್ನು ಚರ್ಚಿಸಲು ಬಯಸಿದರೆ <a href="#" class="support-contact"> ನಮ್ಮನ್ನು ಸಂಪರ್ಕಿಸಿ </a> .';
+$lang['support_app_name'] = 'ಬೆಂಬಲ';
+$lang['support_chat_not_available'] = 'ಚಾಟ್ ಮೂಲಕ ಬೆಂಬಲ ಈ ಸಮಯದಲ್ಲಿ ಲಭ್ಯವಿಲ್ಲ.';
+$lang['support_community_forums'] = 'ಸಮುದಾಯ ವೇದಿಕೆಗಳು';
+$lang['support_contact_upgrades'] = 'ಸಂಪರ್ಕಿಸಿ ನಮ್ಮ <a href="#" class="support-contact"> ಅಪ್ಗ್ರೇಡ್ ತಂಡದ </a> ಆಯ್ಕೆಗಳನ್ನು.';
+$lang['support_day_remaining'] = 'ದಿನ ಉಳಿದ';
+$lang['support_days_remaining'] = 'ಉಳಿದಿರುವ ದಿನಗಳು';
+$lang['support_documentation'] = 'ದಾಖಲೆ';
+$lang['support_knowledge_base'] = 'ಸುಧಾರಿತ ಜ್ಞಾನ ನೆಲೆ';
+$lang['support_learn_more'] = 'ಹೆಚ್ಚು ತಿಳಿಯಿರಿ';
+$lang['support_no_open_tickets'] = 'ಯಾವುದೇ ಓಪನ್ ಟಿಕೆಟ್';
+$lang['support_not_available'] = 'ಲಭ್ಯವಿಲ್ಲ';
+$lang['support_phone'] = 'ದೂರವಾಣಿ';
+$lang['support_please_open_ticket'] = 'ಒಂದು ಚಾಟ್ ಸೆಷನ್ನಲ್ಲಿ ಪ್ರಾರಂಭಿಸುವ ಮೊದಲು ಒಂದು ಬೆಂಬಲ ಟಿಕೆಟ್ ತೆರೆಯಲು ದಯವಿಟ್ಟು.';
+$lang['support_realtime_chat'] = 'ರಿಯಲ್ ಟೈಮ್ ಚಾಟ್ ಬೆಂಬಲ';
+$lang['support_submit_bug_report'] = 'ಒಂದು ದೋಷವನ್ನು ವರದಿ ಸಲ್ಲಿಸಿ';
+$lang['support_submit_ticket'] = 'ಒಂದು ಬೆಂಬಲ ಟಿಕೆಟ್ ಸಲ್ಲಿಸಿ';
+$lang['support_upgrades_and_per_incident'] = 'ಬೆಂಬಲ ಅರ್ಹವಾಗಲು, ಒಂದು ಪ್ರತಿ ಘಟನೆ ಟಿಕೆಟ್ ಖರೀದಿಸಲು ಅಥವಾ ನಿಮ್ಮ ಚಂದಾದಾರಿಕೆಯನ್ನು ಮಟ್ಟದ ಅಪ್ಗ್ರೇಡ್ ಮಾಡಿ <a href="#" class="support-upgrade-url"> ಇಲ್ಲಿ </a> .';
+$lang['support_upgrade'] = 'ಅಪ್ಗ್ರೇಡ್';
+$lang['support_upgrade_window'] = 'ಅರ್ಹತಾ ಅಪ್ಗ್ರೇಡ್';
+$lang['support_web'] = 'ವೆಬ್';
