f28eb467c18e1c75ff504f19cb291bd808501d3a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:02:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'таны системийг дэмжих Сонголтууд болон нөөц. Хэрэв та таны хайсан хэлэлцэх үү хүсэж байгаа бол <a href="#" class="support-contact"> бидэнтэй холбоо барина уу </a> .';
+$lang['support_app_name'] = 'дэмжлэг үзүүлэх';
+$lang['support_chat_not_available'] = 'чат дамжуулан дэмжих энэ үед боломжгүй байна.';
+$lang['support_community_forums'] = 'Олон нийтийн форум';
+$lang['support_contact_upgrades'] = 'Холбоо барих бидний <a href="#" class="support-contact"> шинэчлэх баг </a> тохируулгууд юм.';
+$lang['support_day_remaining'] = 'Үлдсэн өдөр';
+$lang['support_days_remaining'] = 'Үлдсэн өдөр';
+$lang['support_documentation'] = 'Documentation буюу бичиг баримтын';
+$lang['support_knowledge_base'] = 'Нарийвчилсан Мэдлэгийн сан';
+$lang['support_learn_more'] = 'Илүү их судла';
+$lang['support_no_open_tickets'] = 'Нээлттэй хэрэгслээр зорчих тасалбар ямар ч';
+$lang['support_not_available'] = 'бэлэн бус,';
+$lang['support_phone'] = 'Гар утас';
+$lang['support_please_open_ticket'] = 'нь чат хуралдааныг эхлүүлэхээсээ өмнө нь дэмжлэг тасалбар нээж уу.';
+$lang['support_realtime_chat'] = 'Real Time чат дэмжлэг үзүүлэх';
+$lang['support_submit_bug_report'] = 'нь Алдааны тайланг оруулах';
+$lang['support_submit_ticket'] = 'нь дэмжлэг Тасалбар оруулах';
+$lang['support_upgrades_and_per_incident'] = 'дэмжлэг авах эрхтэй байхын тулд нэгж осол тасалбар худалдан авах, эсвэл таны захиалгын түвшинг сайжруулах уу <a href="#" class="support-upgrade-url"> энд </a> .';
+$lang['support_upgrade'] = 'Upgrade';
+$lang['support_upgrade_window'] = 'Эрх шинэчлэх';
+$lang['support_web'] = 'Вэб';
