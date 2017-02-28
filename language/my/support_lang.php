36b77b63bb2e9c3d62b76d5c076fde232a08bc0a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 03:08:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'သင်၏ system ထောက်ခံဘို့အ Options ကိုနှင့်အရင်းအမြစ်များ။ သငျသညျ, သင့်လိုအပ်ချက်များကိုဆွေးနွေးရန်ကျေးဇူးပြုပြီးလိုပါလျှင် <a href="#" class="support-contact"> ကြှနျုပျတို့ကိုဆကျသှယျရနျ </a> ။';
+$lang['support_app_name'] = 'ထောက်ပံ့';
+$lang['support_chat_not_available'] = 'ချက်တင်ကနေတဆင့်ပံ့ပိုးမှုဤအချိန်တွင်ရရှိနိုင်ပါ။';
+$lang['support_community_forums'] = 'ရပ်ရွာဖိုရမ်';
+$lang['support_contact_upgrades'] = 'ကျွန်ုပ်တို့၏ဆက်သွယ်ပါ <a href="#" class="support-contact"> အဖွဲ့အဆင့်မြှင့်တင်ရန် </a> ရွေးချယ်စရာအဘို့။';
+$lang['support_day_remaining'] = 'နေ့လက်ကျန်';
+$lang['support_days_remaining'] = 'လက်ကျန်နေ့ရက်များ';
+$lang['support_documentation'] = 'မှတ်တမ်း';
+$lang['support_knowledge_base'] = 'အဆင့်မြင့်အသိပညာအခြေစိုက်စခန်း';
+$lang['support_learn_more'] = 'ပိုမိုသိရှိရန်';
+$lang['support_no_open_tickets'] = 'အဘယ်သူမျှမပွင့်လင်းလက်မှတ်တွေ';
+$lang['support_not_available'] = 'မရရှိနိုင်ပါ';
+$lang['support_phone'] = 'ဖုန်းနံပါတ်';
+$lang['support_please_open_ticket'] = 'တစ်ဦးကို chat session ကိုစတင်ရှေ့တော်၌ထောက်ခံမှုလက်မှတ်ဖွင့်လှစ်ပေးပါ။';
+$lang['support_realtime_chat'] = 'Real Time Chat ကိုပံ့ပိုးမှု';
+$lang['support_submit_bug_report'] = 'တစ်ဦး Bug အစီရင်ခံစာတင်သွင်းရန်';
+$lang['support_submit_ticket'] = 'တစ်ဦးပံ့ပိုးမှုလက်မှတ် Submit';
+$lang['support_upgrades_and_per_incident'] = 'ထောက်ခံမှုများအတွက်အရည်အချင်းပြည့်မီဖြစ်စေရန်, တစ်ဦးနှုန်း-အဖြစ်အပျက်လက်မှတ်ဝယ်ယူရန်သို့မဟုတ်သင့် subscription ကိုအဆငျ့အဆင့်မြှင့်တင်ရန်ကျေးဇူးပြုပြီး <a href="#" class="support-upgrade-url"> ဒီမှာ </a> ။';
+$lang['support_upgrade'] = 'upgrade';
+$lang['support_upgrade_window'] = 'အရည်အချင်းများကိုအဆင့်မြှင့်တင်ရန်';
+$lang['support_web'] = 'web ကို';
