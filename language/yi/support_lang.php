7184e711ad1a3ab2c54e272d7f154d5c055b2c9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:14:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'אָפּציעס און רעסורסן פֿאַר סופּפּאָרטינג אייער סיסטעם. אויב איר וואָלט ווי צו דיסקוטירן דיין ריקוויירמאַנץ, ביטע <a href="#" class="support-contact"> רוף אונז </a> .';
+$lang['support_app_name'] = 'שטיצן';
+$lang['support_chat_not_available'] = 'שטיצן דורך שמועס איז אַנאַוויילאַבאַל בייַ דעם מאָל.';
+$lang['support_community_forums'] = 'קהל גרופּעס';
+$lang['support_contact_upgrades'] = 'קאָנטאַקט אונדזער <a href="#" class="support-contact"> אַפּגרייד קאָלעקטיוו </a> פֿאַר אָפּציעס.';
+$lang['support_day_remaining'] = 'טאָג רוען';
+$lang['support_days_remaining'] = 'טעג רוען';
+$lang['support_documentation'] = 'דאָקומענטאַטיאָן';
+$lang['support_knowledge_base'] = 'אַוואַנסירטע וויסן באַסע';
+$lang['support_learn_more'] = 'לערן מער';
+$lang['support_no_open_tickets'] = 'ניט קיין עפֿן טיקקעץ';
+$lang['support_not_available'] = 'נישט עוועלעבעל';
+$lang['support_phone'] = 'טעלעפאָנירן';
+$lang['support_please_open_ticket'] = 'ביטע עפענען אַ שטיצן בילעט איידער ינישיייטינג אַ שמועס סעסיע.';
+$lang['support_realtime_chat'] = 'רעאַל צייט טשאַט שטיצן';
+$lang['support_submit_bug_report'] = 'פאָרלייגן אַ זשוק באריכט';
+$lang['support_submit_ticket'] = 'פאָרלייגן אַ שטיצן בילעט';
+$lang['support_upgrades_and_per_incident'] = 'צו זיין פּאַסיק פֿאַר שטיצן, ביטע קויפן אַ פּער-אינצידענט בילעט אָדער אַפּגרייד דיין אַבאָנעמענט מדרגה <a href="#" class="support-upgrade-url"> דאָ </a> .';
+$lang['support_upgrade'] = 'ופּגראַדע';
+$lang['support_upgrade_window'] = 'ופּגראַדע בארעכטיגונג';
+$lang['support_web'] = 'נעץ';
