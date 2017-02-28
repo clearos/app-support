c72d566d2bdc194030646ffbc38dd454376175e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:26:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Функції та ресурси для підтримки системи. Якщо ви хочете, щоб обговорити ваші вимоги, будь ласка, <a href="#" class="support-contact"> Зв&#39;яжіться з нами </a> ,';
+$lang['support_app_name'] = 'підтримка';
+$lang['support_chat_not_available'] = 'Підтримка через чат недоступний в цей час.';
+$lang['support_community_forums'] = 'Форум';
+$lang['support_contact_upgrades'] = 'Зверніться до нашого <a href="#" class="support-contact"> команда поновлення </a> для варіантів.';
+$lang['support_day_remaining'] = 'день Залишилося';
+$lang['support_days_remaining'] = 'Днів, що залишилися';
+$lang['support_documentation'] = 'документація';
+$lang['support_knowledge_base'] = 'Розширений База знань';
+$lang['support_learn_more'] = 'Вивчайте більше';
+$lang['support_no_open_tickets'] = 'Немає відкритих квитків';
+$lang['support_not_available'] = 'Недоступний';
+$lang['support_phone'] = 'телефон';
+$lang['support_please_open_ticket'] = 'Будь ласка, відкрийте квиток підтримки до початку розмови.';
+$lang['support_realtime_chat'] = 'Підтримка в режимі реального часу чат';
+$lang['support_submit_bug_report'] = 'Надіслати звіт про помилку';
+$lang['support_submit_ticket'] = 'Відправити квиток підтримки';
+$lang['support_upgrades_and_per_incident'] = 'Для того, щоб мати право на отримання підтримки, будь ласка, придбати квиток за інцидент або оновити ваш рівень передплати <a href="#" class="support-upgrade-url"> тут </a> ,';
+$lang['support_upgrade'] = 'модернізація';
+$lang['support_upgrade_window'] = 'оновлення правочинності';
+$lang['support_web'] = 'Web';
