97d85cd9dc416836574a7ec6db8a4d512646b357
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:11:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Funkcijos ir ištekliai remti savo sistemą. Jei norite aptarti savo reikalavimus, prašome <a href="#" class="support-contact"> Susisiekite su mumis </a> ,';
+$lang['support_app_name'] = 'parama';
+$lang['support_chat_not_available'] = 'Pagalba per pokalbių yra nepasiekiamas šiuo metu.';
+$lang['support_community_forums'] = 'forumas';
+$lang['support_contact_upgrades'] = 'Susisiekite su mūsų <a href="#" class="support-contact"> atnaujinti komanda </a> pasirinkčių.';
+$lang['support_day_remaining'] = 'dienos Likęs';
+$lang['support_days_remaining'] = 'likusios dienos';
+$lang['support_documentation'] = 'Dokumentacija';
+$lang['support_knowledge_base'] = 'Išplėstinė Žinių bazės';
+$lang['support_learn_more'] = 'Sužinoti daugiau';
+$lang['support_no_open_tickets'] = 'Nėra Open bilietai';
+$lang['support_not_available'] = 'Nepasiekiamas';
+$lang['support_phone'] = 'telefonas';
+$lang['support_please_open_ticket'] = 'Prašome atidaryti paramos bilietą prieš pradedant pokalbių seansą.';
+$lang['support_realtime_chat'] = 'Realaus laiko pokalbių Pagalba';
+$lang['support_submit_bug_report'] = 'Pateikti pranešimą apie ydą';
+$lang['support_submit_ticket'] = 'Pateikti paramos bilietą';
+$lang['support_upgrades_and_per_incident'] = 'Norėdami būti skiriama parama, prašome įsigyti bilietą už incidentą ar atnaujinti jūsų prenumeratos lygio <a href="#" class="support-upgrade-url"> čia </a> ,';
+$lang['support_upgrade'] = 'Patobulinti';
+$lang['support_upgrade_window'] = 'Atnaujinkite Teisė';
+$lang['support_web'] = 'tinklas';
