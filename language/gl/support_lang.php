3a77441861717cd34bbd3134d5f8d7682908727c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 03:22:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Opcións e recursos para apoiar o sistema. Se desexa discutir as súas necesidades, por favor <a href="#" class="support-contact"> Contacta connosco </a> .';
+$lang['support_app_name'] = 'apoio';
+$lang['support_chat_not_available'] = 'Soporte vía chat está dispoñible neste momento.';
+$lang['support_community_forums'] = 'Foros da comunidade';
+$lang['support_contact_upgrades'] = 'póñase en contacto co noso <a href="#" class="support-contact"> equipo de actualización </a> para opcións.';
+$lang['support_day_remaining'] = 'día Permanecendo';
+$lang['support_days_remaining'] = 'días restantes';
+$lang['support_documentation'] = 'documentación';
+$lang['support_knowledge_base'] = 'Base de Coñecemento avanzado';
+$lang['support_learn_more'] = 'Aprender máis';
+$lang['support_no_open_tickets'] = 'Ningúns billetes Abertas';
+$lang['support_not_available'] = 'Non dispoñible';
+$lang['support_phone'] = 'teléfono';
+$lang['support_please_open_ticket'] = 'Por favor, abra un ticket de soporte antes de iniciar unha sesión de chat.';
+$lang['support_realtime_chat'] = 'Soporte chat en tempo real';
+$lang['support_submit_bug_report'] = 'Enviar un informe de erro';
+$lang['support_submit_ticket'] = 'Enviar Ticket Soporte';
+$lang['support_upgrades_and_per_incident'] = 'Para seren elixibles para apoio, por favor, comprar un billete por incidente ou actualizar o seu nivel de subscrición <a href="#" class="support-upgrade-url"> aquí </a> .';
+$lang['support_upgrade'] = 'actualización';
+$lang['support_upgrade_window'] = 'Mellora a elixibilidade';
+$lang['support_web'] = 'tea';
