39ec508c5be34d27c26d325b68679f4ca99c4250
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 03:11:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Zosintha ndi zinthu zothandiza dongosolo wanu. Ngati mukufuna kukambirana zofunika wanu, chonde <a href="#" class="support-contact"> Lumikizanani nafe </a> .';
+$lang['support_app_name'] = 'Support';
+$lang['support_chat_not_available'] = 'Support kudzera macheza sakupezeka pa nthawi ino.';
+$lang['support_community_forums'] = 'Mabwalo Community';
+$lang['support_contact_upgrades'] = 'tinafikira <a href="#" class="support-contact"> gulu Mokweza </a> pakuti options.';
+$lang['support_day_remaining'] = 'tsiku Kukhalabe';
+$lang['support_days_remaining'] = 'masiku Kukhalabe';
+$lang['support_documentation'] = 'Kumasulira';
+$lang['support_knowledge_base'] = 'Zotsogola Knowledge Base';
+$lang['support_learn_more'] = 'Dziwani zambiri';
+$lang['support_no_open_tickets'] = 'No Matikiti Open';
+$lang['support_not_available'] = 'Sakupezeka';
+$lang['support_phone'] = 'Phone';
+$lang['support_please_open_ticket'] = 'Tsegulani ndi thandizo tikiti pamaso kuyambitsa gawo macheza.';
+$lang['support_realtime_chat'] = 'Real Time Chat Support';
+$lang['support_submit_bug_report'] = 'Perekani Bug Report';
+$lang['support_submit_ticket'] = 'Perekani Support Tikiti';
+$lang['support_upgrades_and_per_incident'] = 'Kukhala kulandira thandizo, gulani ndi pa-chochitika chiphaso kapena Sinthani muzimvetsera wanu mlingo <a href="#" class="support-upgrade-url"> Pano </a> .';
+$lang['support_upgrade'] = 'Mokweza';
+$lang['support_upgrade_window'] = 'Sinthani kuvomerezeka';
+$lang['support_web'] = 'Web';
