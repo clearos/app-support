5a6bf7431c218b72e07a9cc3ea1466e9f99c68eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:47:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Opțiuni și resurse pentru susținerea sistemului dumneavoastră. În cazul în care doriți să discutați cerințele dumneavoastră, vă rugăm să <a href="#" class="support-contact"> Contacteaza-ne </a> .';
+$lang['support_app_name'] = 'A sustine';
+$lang['support_chat_not_available'] = 'Suport prin chat-ul nu este disponibilă în acest moment.';
+$lang['support_community_forums'] = 'Forumul comunității';
+$lang['support_contact_upgrades'] = 'contactaţi-ne <a href="#" class="support-contact"> echipa de upgrade </a> pentru opțiuni.';
+$lang['support_day_remaining'] = 'zi rămasă';
+$lang['support_days_remaining'] = 'Zile Ramase';
+$lang['support_documentation'] = 'Documentație';
+$lang['support_knowledge_base'] = 'Baza de cunoștințe avansate';
+$lang['support_learn_more'] = 'Află mai multe';
+$lang['support_no_open_tickets'] = 'Nu există bilete deschise';
+$lang['support_not_available'] = 'Nu e disponibil';
+$lang['support_phone'] = 'Telefon';
+$lang['support_please_open_ticket'] = 'Vă rugăm să deschideți un tichet de suport înainte de a iniția o sesiune de chat.';
+$lang['support_realtime_chat'] = 'În timp real de chat Suport';
+$lang['support_submit_bug_report'] = 'Trimite un raport Bug';
+$lang['support_submit_ticket'] = 'Să prezinte un tichet de suport';
+$lang['support_upgrades_and_per_incident'] = 'Pentru a fi eligibil pentru sprijin, vă rugăm să achiziționați un bilet per incident sau upgrade nivelul abonamentului <a href="#" class="support-upgrade-url"> aici </a> .';
+$lang['support_upgrade'] = 'Actualizare';
+$lang['support_upgrade_window'] = 'eligibilitate pentru upgrade';
+$lang['support_web'] = 'Web';
