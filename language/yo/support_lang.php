ca31050a04fba89ddf10f61a80fe79a553bec86b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:13:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Aṣayan ati oro fun atilẹyin rẹ eto. Ti o ba ti o ba yoo fẹ lati jiroro awọn ibeere rẹ, jọwọ <a href="#" class="support-contact"> pe wa </a> .';
+$lang['support_app_name'] = 'support';
+$lang['support_chat_not_available'] = 'Support nipasẹ iwiregbe ni miiran ko ni akoko yi.';
+$lang['support_community_forums'] = 'Community Forums';
+$lang['support_contact_upgrades'] = 'kan si wa <a href="#" class="support-contact"> igbesoke egbe </a> fun awọn aṣayan.';
+$lang['support_day_remaining'] = 'Day o ku';
+$lang['support_days_remaining'] = 'ọjọ ku';
+$lang['support_documentation'] = 'iwe';
+$lang['support_knowledge_base'] = 'To ti ni ilọsiwaju Àkójọpọ Ìmọ';
+$lang['support_learn_more'] = 'Kọ ẹkọ diẹ si';
+$lang['support_no_open_tickets'] = 'Ko si Open Tiketi';
+$lang['support_not_available'] = 'Ko si';
+$lang['support_phone'] = 'foonu';
+$lang['support_please_open_ticket'] = 'Jọwọ ṣi a support tiketi ṣaaju ki o to pilẹìgbàlà a iwiregbe igba.';
+$lang['support_realtime_chat'] = 'Real Time Awo Support';
+$lang['support_submit_bug_report'] = 'Fi kan Bug Report';
+$lang['support_submit_ticket'] = 'Fi a Support tiketi';
+$lang['support_upgrades_and_per_incident'] = 'Lati le yẹ fun support, jọwọ ra a fun-isẹlẹ tiketi tabi igbesoke alabapin rẹ ipele <a href="#" class="support-upgrade-url"> Nibi </a> .';
+$lang['support_upgrade'] = 'igbesoke';
+$lang['support_upgrade_window'] = 'igbesoke yiyẹ ni';
+$lang['support_web'] = 'ayelujara';
