320b50eee4c546fa492c2ba7c3e43ff75b489b22
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:34:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Pilihan jeung sumber pikeun ngarojong sistim Anjeun. Lamun hoyong ngabahas syarat, mangga <a href="#" class="support-contact"> taros Kami </a> .';
+$lang['support_app_name'] = 'sokongan';
+$lang['support_chat_not_available'] = 'Rojongan via chat anu sadia dina waktu ieu.';
+$lang['support_community_forums'] = 'Sawala Community';
+$lang['support_contact_upgrades'] = 'Kontak urang <a href="#" class="support-contact"> tim pamutahiran </a> keur pilihan.';
+$lang['support_day_remaining'] = 'poé Sésana';
+$lang['support_days_remaining'] = 'days Sésana';
+$lang['support_documentation'] = 'dokuméntasi';
+$lang['support_knowledge_base'] = 'Base Pangaweruh Advanced';
+$lang['support_learn_more'] = 'Diajar deui';
+$lang['support_no_open_tickets'] = 'No Open Tiket';
+$lang['support_not_available'] = 'Henteu sayogi';
+$lang['support_phone'] = 'Phone';
+$lang['support_please_open_ticket'] = 'Mangga muka tiket pangrojong saméméh initiating sési chat.';
+$lang['support_realtime_chat'] = 'Real Time Chat Rojongan';
+$lang['support_submit_bug_report'] = 'Nepikeun hiji kutu Laporan';
+$lang['support_submit_ticket'] = 'Nepikeun hiji tiket Rojongan';
+$lang['support_upgrades_and_per_incident'] = 'Jadi layak pikeun pangrojong, mangga meuli tiket per-kajadian atawa ningkatkeun tingkat langganan Anjeun <a href="#" class="support-upgrade-url"> Ieuh </a> .';
+$lang['support_upgrade'] = 'pamutahiran';
+$lang['support_upgrade_window'] = 'ningkatkeun eligibility';
+$lang['support_web'] = 'web';
