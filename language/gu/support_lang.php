c742ea0dd548edad906eace2e8d0953ac22e78c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 03:26:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'વિકલ્પો અને તમારી સિસ્ટમ સહાયક માટે સંસાધનો. તમે તમારી જરૂરિયાતો માટે, કૃપા કરીને ચર્ચા કરવા માંગો છો, તો <a href="#" class="support-contact"> અમારો સંપર્ક કરો </a> .';
+$lang['support_app_name'] = 'આધાર';
+$lang['support_chat_not_available'] = 'ચેટ મારફતે આધાર આ સમયે અનુપલબ્ધ છે.';
+$lang['support_community_forums'] = 'કોમ્યુનિટી ફોરમ્સ';
+$lang['support_contact_upgrades'] = 'સંપર્ક અમારા <a href="#" class="support-contact"> સુધારો ટીમ </a> વિકલ્પો છે.';
+$lang['support_day_remaining'] = 'દિવસ બાકી';
+$lang['support_days_remaining'] = 'બાકી દિવસો';
+$lang['support_documentation'] = 'દસ્તાવેજીકરણ';
+$lang['support_knowledge_base'] = 'ઉન્નત નોલેજ બેઝ';
+$lang['support_learn_more'] = 'વધુ શીખો';
+$lang['support_no_open_tickets'] = 'કોઈ ઓપન ટિકિટ';
+$lang['support_not_available'] = 'ઉપલબ્ધ નથી';
+$lang['support_phone'] = 'ફોન';
+$lang['support_please_open_ticket'] = 'કૃપા કરીને ચેટ સત્ર શરૂ કરો તે પહેલાં એક આધાર ટિકિટ ખોલવા.';
+$lang['support_realtime_chat'] = 'પ્રત્યક્ષ સમયનો ચેટ આધાર';
+$lang['support_submit_bug_report'] = 'ક્ષતિ અહેવાલ સબમિટ';
+$lang['support_submit_ticket'] = 'એક આધાર ટિકિટ સબમિટ';
+$lang['support_upgrades_and_per_incident'] = 'આધાર માટે પાત્ર થવા માટે, પ્રતિ ઘટના ટિકિટ ખરીદી અથવા તમારા ઉમેદવારી સ્તર અપગ્રેડ કરો <a href="#" class="support-upgrade-url"> અહીં </a> .';
+$lang['support_upgrade'] = 'સુધારો';
+$lang['support_upgrade_window'] = 'સુધારો લાયકાત';
+$lang['support_web'] = 'વેબ';
