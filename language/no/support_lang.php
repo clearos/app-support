c3c9760c81cf368b8f2d6913d7d38018c3a1d7e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:54:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Valg og ressurser for å støtte systemet. Hvis du ønsker å diskutere dine behov, kan du <a href="#" class="support-contact"> kontakt oss </a> .';
+$lang['support_app_name'] = 'Brukerstøtte';
+$lang['support_chat_not_available'] = 'Støtte via chat er ikke tilgjengelig på dette tidspunktet.';
+$lang['support_community_forums'] = 'Forum';
+$lang['support_contact_upgrades'] = 'Kontakt vår <a href="#" class="support-contact"> oppgradering lag </a> for alternativer.';
+$lang['support_day_remaining'] = 'dag igjen';
+$lang['support_days_remaining'] = 'Dager igjen';
+$lang['support_documentation'] = 'dokumentasjon';
+$lang['support_knowledge_base'] = 'Avansert Knowledge Base';
+$lang['support_learn_more'] = 'Lære mer';
+$lang['support_no_open_tickets'] = 'Ingen Open billetter';
+$lang['support_not_available'] = 'Ikke tilgjengelig';
+$lang['support_phone'] = 'telefon';
+$lang['support_please_open_ticket'] = 'Vennligst åpne en support billett før du starter en chat.';
+$lang['support_realtime_chat'] = 'Real Time Chat Support';
+$lang['support_submit_bug_report'] = 'Send inn en feilrapport';
+$lang['support_submit_ticket'] = 'Send inn en Support Ticket';
+$lang['support_upgrades_and_per_incident'] = 'For å være kvalifisert for støtte, kan du kjøpe en per hendelse billett eller oppgradere abonnementet nivå <a href="#" class="support-upgrade-url"> her </a> .';
+$lang['support_upgrade'] = 'Oppgradering';
+$lang['support_upgrade_window'] = 'Oppgrader Kvalifikasjon';
+$lang['support_web'] = 'web';
