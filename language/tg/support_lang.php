4589ee0424efe9db9a474230305c8036c2321c79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:30:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Имконот ва захираҳо барои дастгирии системаи шумо. Агар шумо мехоҳед ба муҳокима талаботи худ, лутфан <a href="#" class="support-contact"> бо мо тамос гиред </a> .';
+$lang['support_app_name'] = 'таъмин';
+$lang['support_chat_not_available'] = 'Дастгирии тавассути Чат дар ин вақт ғайриимкон аст.';
+$lang['support_community_forums'] = 'Форумҳо ҷамоатҳо';
+$lang['support_contact_upgrades'] = 'Иртибот бо мо <a href="#" class="support-contact"> дастаи азнавсозӣ </a> барои имконоти.';
+$lang['support_day_remaining'] = 'рӯзи боқимонда';
+$lang['support_days_remaining'] = 'Рӯзҳои боқимонда';
+$lang['support_documentation'] = 'Ҳуҷҷатгузорӣ';
+$lang['support_knowledge_base'] = 'Пойгоҳи Дониш Advanced';
+$lang['support_learn_more'] = 'Бисёр омӯхтан';
+$lang['support_no_open_tickets'] = 'Не Чиптаҳои Open';
+$lang['support_not_available'] = 'Дастрас нест';
+$lang['support_phone'] = 'телефон';
+$lang['support_please_open_ticket'] = 'Лутфан як чипта дастгирии пеш ташаббускори як ҷаласаи Чат кушоянд.';
+$lang['support_realtime_chat'] = 'Вақти воқеӣ Дастгирии Чат';
+$lang['support_submit_bug_report'] = 'Пайём гузориш оиди хатои система';
+$lang['support_submit_ticket'] = 'Пешниҳоди чипта Дастгирии';
+$lang['support_upgrades_and_per_incident'] = 'Барои ҳуқуқ барои дастгирии, лутфан харидани чипта як-ҳодисаҳо ва ё навсозӣ сатҳи обунаи шуморо дар <a href="#" class="support-upgrade-url"> Ин ҷо </a> .';
+$lang['support_upgrade'] = 'навсозӣ';
+$lang['support_upgrade_window'] = 'Талаботи навсозӣ';
+$lang['support_web'] = 'Web';
