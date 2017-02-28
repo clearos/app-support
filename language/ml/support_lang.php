59f9f8b620790db88b7b6a423852e7b84b2c9896
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 05:03:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'നിങ്ങളുടെ സിസ്റ്റം പിന്തുണച്ചതിന് ഓപ്ഷനുകൾ ഉറവിടങ്ങളും. ദയവു ചെയ്ത്, നിങ്ങളുടെ ആവശ്യകതകൾ ചർച്ച ചെയ്യാൻ ആഗ്രഹിക്കുന്നുവെങ്കിൽ <a href="#" class="support-contact"> ഞങ്ങളെ സമീപിക്കുക </a> .';
+$lang['support_app_name'] = 'പിന്തുണ';
+$lang['support_chat_not_available'] = 'ചാറ്റ് വഴി പിന്തുണ ഈ സമയം ലഭ്യമല്ല.';
+$lang['support_community_forums'] = 'കമ്മ്യൂണിറ്റി ഫോറങ്ങൾ';
+$lang['support_contact_upgrades'] = 'ഞങ്ങളുടെ കോൺടാക്റ്റ് <a href="#" class="support-contact"> ടീം അപ്ഗ്രേഡ് </a> ഓപ്ഷനുകൾ വേണ്ടി.';
+$lang['support_day_remaining'] = 'ദിവസം ശേഷിക്കുന്നു';
+$lang['support_days_remaining'] = 'ശേഷിക്കുന്ന ദിനങ്ങൾ';
+$lang['support_documentation'] = 'വിവരണക്കുറിപ്പു്';
+$lang['support_knowledge_base'] = 'വിപുലമായ ബേസ്';
+$lang['support_learn_more'] = 'കൂടുതലറിവ് നേടുക';
+$lang['support_no_open_tickets'] = 'തുറക്കുക ടിക്കറ്റുകൾ ഇല്ല';
+$lang['support_not_available'] = 'ലഭ്യമല്ല';
+$lang['support_phone'] = 'ഫോൺ';
+$lang['support_please_open_ticket'] = 'ഒരു ചാറ്റ് സെഷൻ തുടങ്ങുന്നതിന് മുമ്പായി ഒരു പിന്തുണ ടിക്കറ്റ് തുറക്കുക.';
+$lang['support_realtime_chat'] = 'റിയൽ ടൈം ചാറ്റ് പിന്തുണ';
+$lang['support_submit_bug_report'] = 'ബഗ് റിപ്പോർട്ട് സമർപ്പിക്കുക';
+$lang['support_submit_ticket'] = 'ഒരു പിന്തുണ ടിക്കറ്റ് സമർപ്പിക്കുക';
+$lang['support_upgrades_and_per_incident'] = 'പിന്തുണയ്ക്ക് യോഗ്യത നേടുന്നതിന്, ഓരോ സംഭവം ടിക്കറ്റ് വാങ്ങുക അല്ലെങ്കിൽ നിങ്ങളുടെ സബ്സ്ക്രിപ്ഷൻ ലെവൽ അപ്ഗ്രേഡ് <a href="#" class="support-upgrade-url"> ഇവിടെ </a> .';
+$lang['support_upgrade'] = 'നവീകരിക്കുക';
+$lang['support_upgrade_window'] = 'യോഗ്യത അപ്ഗ്രേഡ്';
+$lang['support_web'] = 'വെബ്';
