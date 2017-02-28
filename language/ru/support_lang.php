bed6323836787d1352f94f3c25483bd83bb2e75f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:46:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Функции и ресурсы для поддержки системы. Если вы хотите, чтобы обсудить ваши требования, пожалуйста, <a href="#" class="support-contact"> свяжитесь с нами </a> ,';
+$lang['support_app_name'] = 'Поддержка';
+$lang['support_chat_not_available'] = 'Поддержка через чат недоступен в это время.';
+$lang['support_community_forums'] = 'Форум';
+$lang['support_contact_upgrades'] = 'Обратитесь к нашему <a href="#" class="support-contact"> команда обновления </a> для вариантов.';
+$lang['support_day_remaining'] = 'День Оставшееся';
+$lang['support_days_remaining'] = 'Осталось дней';
+$lang['support_documentation'] = 'Документация';
+$lang['support_knowledge_base'] = 'Расширенный База знаний';
+$lang['support_learn_more'] = 'Выучить больше';
+$lang['support_no_open_tickets'] = 'Нет открытых билетов';
+$lang['support_not_available'] = 'Недоступен';
+$lang['support_phone'] = 'Телефон';
+$lang['support_please_open_ticket'] = 'Пожалуйста, откройте билет поддержки до начала сеанса чата.';
+$lang['support_realtime_chat'] = 'Поддержка в режиме реального времени чат';
+$lang['support_submit_bug_report'] = 'Отправить отчет об ошибке';
+$lang['support_submit_ticket'] = 'Отправить билет поддержки';
+$lang['support_upgrades_and_per_incident'] = 'Для того, чтобы иметь право на получение поддержки, пожалуйста, приобрести билет за инцидент или обновить ваш уровень подписки <a href="#" class="support-upgrade-url"> Вот </a> ,';
+$lang['support_upgrade'] = 'Обновить';
+$lang['support_upgrade_window'] = 'Обновление правомочности';
+$lang['support_web'] = 'Web';
