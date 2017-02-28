df479ec9a089d0b29826c355828e43c22f3d25da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-28 04:12:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Izinketho nezisetshenziswa nesimiso sakho. Uma ungathanda ukudingida izidingo zakho, sicela <a href="#" class="support-contact"> Xhumana nathi </a> .';
+$lang['support_app_name'] = 'Amaprosesa asekelwe';
+$lang['support_chat_not_available'] = 'Ukusekela nge chat ayitholakali ngalesi sikhathi.';
+$lang['support_community_forums'] = 'Forums Community';
+$lang['support_contact_upgrades'] = 'Xhumana yethu <a href="#" class="support-contact"> ithimba kubuyekeze </a> ngokukhethwa kukho.';
+$lang['support_day_remaining'] = 'Usuku Ukuhlala';
+$lang['support_days_remaining'] = 'Izinsuku Ukuhlala';
+$lang['support_documentation'] = 'Amaphepha';
+$lang['support_knowledge_base'] = 'Advanced Isizinda Solwazi';
+$lang['support_learn_more'] = 'Funda kabanzi';
+$lang['support_no_open_tickets'] = 'Ayikho Amathikithi Vula';
+$lang['support_not_available'] = 'Akutholakali';
+$lang['support_phone'] = 'Hambayo';
+$lang['support_please_open_ticket'] = 'Sicela uvule ithikithi support ngaphambi kokuba uqale session lengxoxo.';
+$lang['support_realtime_chat'] = 'Isikhathi Real ukweseka Chat';
+$lang['support_submit_bug_report'] = 'Hambisa Umbiko Bug';
+$lang['support_submit_ticket'] = 'Hambisa ukweseka iThikithi';
+$lang['support_upgrades_and_per_incident'] = 'Ukuze ufaneleke ukuthola ukwesekwa, sicela athenge ithikithi ngalinye-isigameko noma uthuthukise okubhaliselwe wakho ezingeni <a href="#" class="support-upgrade-url"> lapha </a> .';
+$lang['support_upgrade'] = 'Upgrade';
+$lang['support_upgrade_window'] = 'Thuthukisa Ukufaneleka';
+$lang['support_web'] = 'Web';
