8322a8b068324f14f77187d076458a1d65c2561d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:14:14 +0000

    Add new files and directories, commit changes, push changes

--- language/mg/support_lang.php
+++ language/mg/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Options sy loharanon-karena noho ny fanohanany ny rafitra. Raha te-hiresaka momba ny zavatra takiana, azafady <a href="#" class="support-contact"> Mifandraisa aminay </a> .';
+$lang['support_app_description'] = 'Safidy sy loharano ho fanohanana ny rafitrao. Raha te hiresaka momba ny zavatra takinao ianao, azafady <a href="#" class="support-contact"> Mifandraisa aminay </a> .';
 $lang['support_app_name'] = 'Support';
-$lang['support_chat_not_available'] = 'Support alalan&#39;ny Tsy vonona amin&#39;ny chat dia amin&#39;izao fotoana.';
+$lang['support_chat_not_available'] = 'Tsy misy fanohanana amin&#39;ny chat intsony amin&#39;izao fotoana izao.';
 $lang['support_community_forums'] = 'Community Forums';
-$lang['support_contact_upgrades'] = 'Contact ny <a href="#" class="support-contact"> fanavaozana ekipa </a> ny safidy.';
-$lang['support_day_remaining'] = 'Day Ny fijanonana';
-$lang['support_days_remaining'] = 'Days Ny fijanonana';
+$lang['support_contact_upgrades'] = 'Contact our <a href="#" class="support-contact"> ekipa fampandrosoana </a> ho an&#39;ny safidy.';
+$lang['support_day_remaining'] = 'Andro sisa';
+$lang['support_days_remaining'] = 'Andro sisa';
 $lang['support_documentation'] = 'Documentation';
-$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
+$lang['support_knowledge_base'] = 'Fahaizana fahalalana misimisy kokoa';
 $lang['support_learn_more'] = 'Hamantatra bebe kokoa';
 $lang['support_no_open_tickets'] = 'No Open Tickets';
 $lang['support_not_available'] = 'Tsy misy';
 $lang['support_phone'] = 'Phone';
-$lang['support_please_open_ticket'] = 'Azafady hanokatra ny fanohanana tapakila talohan&#39;ny fivoriana natombony ny amin&#39;ny chat.';
+$lang['support_please_open_ticket'] = 'Azafady mba manokafa tapakila fanohanana alohan&#39;ny hanombohana seho an-tsehatra.';
 $lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Manolotra ny Bug Report';
-$lang['support_submit_ticket'] = 'Manolotra ny Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'Mba ho afaka ny fanohanana, azafady hividy tapakila isan-zava-nitranga, na hanatsarana ny famandrihana haavon&#39;ny <a href="#" class="support-upgrade-url"> Eto </a> .';
+$lang['support_submit_bug_report'] = 'Ampidiro ny tatitra iray diso';
+$lang['support_submit_ticket'] = 'Ampidiro ny taratasy fanamafisana';
+$lang['support_upgrades_and_per_incident'] = 'Azonao atao ny mividy tapakila mitentina isan-karazany na manatsara ny sandan&#39;ny subscription anao raha te hahazo ny fanohanana <a href="#" class="support-upgrade-url"> Eto </a> .';
 $lang['support_upgrade'] = 'fanavaozana';
-$lang['support_upgrade_window'] = 'hanatsarana ireo taratasim-panamarinanao';
+$lang['support_upgrade_window'] = 'Upgrade Eligibility';
 $lang['support_web'] = 'Web';
