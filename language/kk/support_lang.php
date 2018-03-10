2a37b8d49da9892f824ab87249f4df771b02279f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:09:10 +0000

    Add new files and directories, commit changes, push changes

--- language/kk/support_lang.php
+++ language/kk/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'жүйеңізді қолдау үшін Опциялар мен ресурстар. Егер сіз, сіздің талаптарды талқылау үшін хабарласыңыз келсе <a href="#" class="support-contact"> бізбен хабарласыңы </a> .';
-$lang['support_app_name'] = 'қолдау';
-$lang['support_chat_not_available'] = 'чат арқылы қолдау осы уақытта қол жетімді болып табылады.';
-$lang['support_community_forums'] = 'Қауымдастық Форумдар';
-$lang['support_contact_upgrades'] = 'Біздің байланыс <a href="#" class="support-contact"> командасын жаңарту </a> опциялары үшін.';
-$lang['support_day_remaining'] = 'күні қалған';
-$lang['support_days_remaining'] = 'қалған күндері';
+$lang['support_app_description'] = 'Жүйеңізді қолдау үшін параметрлер мен ресурстар. Егер сіз өзіңіздің талаптарыңызды талқылағыңыз келсе, өтінеміз <a href="#" class="support-contact"> бізбен хабарласыңы </a> .';
+$lang['support_app_name'] = 'Қолдау';
+$lang['support_chat_not_available'] = 'Қазіргі уақытта чат арқылы қолдау қол жетімді емес.';
+$lang['support_community_forums'] = 'Қоғамдық форумдар';
+$lang['support_contact_upgrades'] = 'Бізбен хабарласыңыз <a href="#" class="support-contact"> жаңарту тобы </a> опциялар үшін.';
+$lang['support_day_remaining'] = 'Қалған күн';
+$lang['support_days_remaining'] = 'Қалған күндер';
 $lang['support_documentation'] = 'Құжаттама';
-$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
+$lang['support_knowledge_base'] = 'Білімді білім базасы';
 $lang['support_learn_more'] = 'Көбірек білу үшін';
-$lang['support_no_open_tickets'] = 'Жоқ Ашық Билеттер';
+$lang['support_no_open_tickets'] = 'Ашық билеттер жоқ';
 $lang['support_not_available'] = 'Жоқ';
 $lang['support_phone'] = 'Телефон';
-$lang['support_please_open_ticket'] = 'сессиясын чат басталар алдында қолдау билет ашу сұраймыз.';
-$lang['support_realtime_chat'] = 'Real Time Чат қолдау';
-$lang['support_submit_bug_report'] = 'қателер баяндама ұсынады';
-$lang['support_submit_ticket'] = 'Қолдау өтінішін жіберген';
-$lang['support_upgrades_and_per_incident'] = 'қолдауға ие бола үшін, бір-оқиғадан билет сатып алу, немесе өзіңіздің жазылым деңгейін жаңарту сұраймыз <a href="#" class="support-upgrade-url"> Мұнда </a> .';
+$lang['support_please_open_ticket'] = 'Сөйлесу сеансын бастаудан бұрын қолдау билетін ашыңыз.';
+$lang['support_realtime_chat'] = 'Нақты уақыттағы чатқа қолдау көрсету';
+$lang['support_submit_bug_report'] = 'Қате туралы есеп беру';
+$lang['support_submit_ticket'] = 'Қолдау билетін жіберу';
+$lang['support_upgrades_and_per_incident'] = 'Қолдау үшін жарамды болу үшін, әр оқиғаға арналған билетті сатып алыңыз немесе жазылым деңгейіңізді жаңартыңыз <a href="#" class="support-upgrade-url"> Мұнда </a> .';
 $lang['support_upgrade'] = 'Жаңалау';
-$lang['support_upgrade_window'] = 'Жарамдылық жаңарту';
+$lang['support_upgrade_window'] = 'Құқықтықты жаңарту';
 $lang['support_web'] = 'желі';
