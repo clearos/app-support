18828422d9266f9fe0d456f17985d06d482e3070
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:03:46 +0000

    Add new files and directories, commit changes, push changes

--- language/hu/support_lang.php
+++ language/hu/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Lehetőségek és erőforrásokat támogató rendszer. Ha szeretné, hogy megvitassák a követelményeknek, kérjük <a href="#" class="support-contact"> lépjen kapcsolatba velünk </a> .';
+$lang['support_app_description'] = 'Opciók és erőforrások a rendszer támogatásához. Ha szeretné megbeszélni az Ön igényeit, kérem <a href="#" class="support-contact"> lépjen kapcsolatba velünk </a> .';
 $lang['support_app_name'] = 'Támogatás';
-$lang['support_chat_not_available'] = 'Support chates nem érhető el ebben az időben.';
-$lang['support_community_forums'] = 'közösségi Fórum';
-$lang['support_contact_upgrades'] = 'Vegye fel a kapcsolatot <a href="#" class="support-contact"> frissítési csapat </a> opciókat.';
-$lang['support_day_remaining'] = 'nap Hátralévő';
+$lang['support_chat_not_available'] = 'A csevegés támogatása jelenleg nem érhető el.';
+$lang['support_community_forums'] = 'Közösségi fórumok';
+$lang['support_contact_upgrades'] = 'Lépjen kapcsolatba velünk <a href="#" class="support-contact"> frissítési csapat </a> az opciókhoz.';
+$lang['support_day_remaining'] = 'Nap maradt';
 $lang['support_days_remaining'] = 'hátralevő napok';
 $lang['support_documentation'] = 'Dokumentáció';
-$lang['support_knowledge_base'] = 'Részletes tudásbázis';
+$lang['support_knowledge_base'] = 'Haladó Tudásbázis';
 $lang['support_learn_more'] = 'Tudj meg többet';
-$lang['support_no_open_tickets'] = 'Nincs Nyitott Jegyek';
+$lang['support_no_open_tickets'] = 'Nincs nyitott jegy';
 $lang['support_not_available'] = 'Nem elérhető';
 $lang['support_phone'] = 'Telefon';
-$lang['support_please_open_ticket'] = 'Kérjük, nyissa meg a támogató jegyet megkezdése előtt csevegés.';
-$lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Egy hibajelentést';
-$lang['support_submit_ticket'] = 'Leadása Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'Ahhoz, hogy jogosult a támogatásra, kérjük, vásárolja meg egy-egy esemény jegy vagy frissíteni az előfizetés szinten <a href="#" class="support-upgrade-url"> itt </a> .';
+$lang['support_please_open_ticket'] = 'Nyissa meg a támogatási jegyet a csevegés megkezdése előtt.';
+$lang['support_realtime_chat'] = 'Valós idejű chat támogatás';
+$lang['support_submit_bug_report'] = 'Hibajelentés küldése';
+$lang['support_submit_ticket'] = 'Támogatási jegy beadása';
+$lang['support_upgrades_and_per_incident'] = 'Ahhoz, hogy jogosult legyen a támogatásra, kérjük, jegyezzen be egy esetleges jegyet vagy frissítse előfizetési szintjét <a href="#" class="support-upgrade-url"> itt </a> .';
 $lang['support_upgrade'] = 'frissítés';
 $lang['support_upgrade_window'] = 'Frissítési jogosultság';
 $lang['support_web'] = 'háló';
