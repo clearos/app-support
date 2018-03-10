52ca47bcef877c8cb4242524117cf227360fe792
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 09:56:04 +0000

    Add new files and directories, commit changes, push changes

--- language/da/support_lang.php
+++ language/da/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Indstillinger og ressourcer til at støtte dit system. Hvis du ønsker at drøfte dine behov, skal du <a href="#" class="support-contact"> kontakt os </a> .';
+$lang['support_app_description'] = 'Valg og ressourcer til at understøtte dit system. Hvis du vil diskutere dine krav, bedes du venligst <a href="#" class="support-contact"> kontakt os </a> .';
 $lang['support_app_name'] = 'Support';
-$lang['support_chat_not_available'] = 'Support via chat er ikke tilgængelig på dette tidspunkt.';
-$lang['support_community_forums'] = 'Fora';
-$lang['support_contact_upgrades'] = 'Kontakt vores <a href="#" class="support-contact"> opgradering hold </a> for valgmuligheder.';
-$lang['support_day_remaining'] = 'Day Resterende';
+$lang['support_chat_not_available'] = 'Support via chat er ikke tilgængelig på nuværende tidspunkt.';
+$lang['support_community_forums'] = 'Fællesskabsfora';
+$lang['support_contact_upgrades'] = 'Kontakt vores <a href="#" class="support-contact"> opgraderingshold </a> for muligheder.';
+$lang['support_day_remaining'] = 'Dag resterende';
 $lang['support_days_remaining'] = 'Dage tilbage';
 $lang['support_documentation'] = 'Dokumentation';
-$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
+$lang['support_knowledge_base'] = 'Avanceret vidensbase';
 $lang['support_learn_more'] = 'Lær mere';
-$lang['support_no_open_tickets'] = 'Ingen Open Billetter';
+$lang['support_no_open_tickets'] = 'Ingen åbne billetter';
 $lang['support_not_available'] = 'Ikke tilgængelig';
 $lang['support_phone'] = 'telefon';
-$lang['support_please_open_ticket'] = 'Åbn en støtte billet, før du starter en chat-session.';
+$lang['support_please_open_ticket'] = 'Åbn en supportbillet, før du starter en chat-session.';
 $lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Indsende en fejlrapport';
-$lang['support_submit_ticket'] = 'Indsend en Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'For at være berettiget til støtte, skal du købe en per-hændelse billet eller opgradere dit abonnement niveau <a href="#" class="support-upgrade-url"> her </a> .';
-$lang['support_upgrade'] = 'Upgrade';
-$lang['support_upgrade_window'] = 'Opgrader Berettigelse';
+$lang['support_submit_bug_report'] = 'Indsend en fejlrapport';
+$lang['support_submit_ticket'] = 'Indsend en support billet';
+$lang['support_upgrades_and_per_incident'] = 'For at være berettiget til support, køb venligst en eventuel billet eller opgrader dit abonnementsniveau <a href="#" class="support-upgrade-url"> her </a> .';
+$lang['support_upgrade'] = 'opgradering';
+$lang['support_upgrade_window'] = 'Opgraderingsberettigelse';
 $lang['support_web'] = 'Web';
