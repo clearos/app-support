1510b250e1406ca675f505ce2f85d996de1ba70d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:21:50 +0000

    Add new files and directories, commit changes, push changes

--- language/fa/support_lang.php
+++ language/fa/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'گزینه ها و منابع برای حمایت از سیستم شما. اگر شما می خواهم تا در مورد نیازهای خود، لطفا <a href="#" class="support-contact"> تماس با ما </a> .';
+$lang['support_app_description'] = 'گزینه ها و منابع برای حمایت از سیستم شما. اگر می خواهید مورد نیاز خود را مورد بحث قرار دهید، لطفا <a href="#" class="support-contact"> تماس با ما </a> .';
 $lang['support_app_name'] = 'حمایت کردن';
 $lang['support_chat_not_available'] = 'پشتیبانی از طریق چت در این زمان در دسترس نیست.';
-$lang['support_community_forums'] = 'انجمن';
-$lang['support_contact_upgrades'] = 'تماس با ما <a href="#" class="support-contact"> تیم ارتقاء </a> برای گزینه.';
+$lang['support_community_forums'] = 'انجمن های انجمن';
+$lang['support_contact_upgrades'] = 'با ما تماس بگیرید <a href="#" class="support-contact"> تیم را ارتقا دهید </a> برای گزینه ها';
 $lang['support_day_remaining'] = 'روز باقی مانده';
 $lang['support_days_remaining'] = 'روزهای باقی مانده';
 $lang['support_documentation'] = 'مستندات';
-$lang['support_knowledge_base'] = 'پایگاه دانش و جوی پیشرفته';
+$lang['support_knowledge_base'] = 'پایگاه دانش پیشرفته';
 $lang['support_learn_more'] = 'بیشتر بدانید';
-$lang['support_no_open_tickets'] = 'بلیط گسترش';
+$lang['support_no_open_tickets'] = 'بدون بلیط های باز';
 $lang['support_not_available'] = 'در دسترس نیست';
 $lang['support_phone'] = 'تلفن';
-$lang['support_please_open_ticket'] = 'لطفا یک بلیط پشتیبانی قبل از شروع یک جلسه چت را باز کنید.';
+$lang['support_please_open_ticket'] = 'قبل از شروع یک جلسه چت، یک بلیط پشتیبانی را باز کنید.';
 $lang['support_realtime_chat'] = 'زمان واقعی پشتیبانی چت';
-$lang['support_submit_bug_report'] = 'ثبت گزارش اشکال';
-$lang['support_submit_ticket'] = 'ارسال درخواست پشتیبانی';
-$lang['support_upgrades_and_per_incident'] = 'برای اینکه واجد شرایط برای پشتیبانی، لطفا خرید یک بلیط در هر حادثه و یا ارتقاء سطح اشتراک خود را <a href="#" class="support-upgrade-url"> اینجا </a> .';
-$lang['support_upgrade'] = 'ارتقا';
-$lang['support_upgrade_window'] = 'ارتقا واجد شرایط';
+$lang['support_submit_bug_report'] = 'گزارش اشکال ارسال کنید';
+$lang['support_submit_ticket'] = 'یک بلیط پشتیبانی ارسال کنید';
+$lang['support_upgrades_and_per_incident'] = 'برای واجد شرایط بودن برای پشتیبانی، لطفا یک بلیط در هر حادثه خرید کنید یا سطح اشتراک خود را ارتقا دهید <a href="#" class="support-upgrade-url"> اینجا </a> .';
+$lang['support_upgrade'] = 'ارتقاء';
+$lang['support_upgrade_window'] = 'ارتقاء الزامات';
 $lang['support_web'] = 'وب';
