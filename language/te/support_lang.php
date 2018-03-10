bd75997b5bc9793b945717d6d35cf1bfad120921
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:32:08 +0000

    Add new files and directories, commit changes, push changes

--- language/te/support_lang.php
+++ language/te/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'ఎంపికలు మరియు మీ సిస్టమ్ మద్దతు కోసం వనరులు. మీరు మీ అవసరాలు చర్చించడానికి దయచేసి కావాలనుకుంటే <a href="#" class="support-contact"> మమ్మల్ని సంప్రదించండి </a> .';
+$lang['support_app_description'] = 'మీ సిస్టమ్కు మద్దతు ఇచ్చే ఐచ్ఛికాలు మరియు వనరులు. మీరు మీ అవసరాలను చర్చించాలనుకుంటే, దయచేసి <a href="#" class="support-contact"> మమ్మల్ని సంప్రదించండి </a> .';
 $lang['support_app_name'] = 'మద్దతు';
 $lang['support_chat_not_available'] = 'చాట్ ద్వారా మద్దతు ఈ సమయంలో అందుబాటులో లేదు.';
 $lang['support_community_forums'] = 'కమ్యూనిటీ ఫోరమ్స్';
-$lang['support_contact_upgrades'] = 'సంప్రదించండి మన <a href="#" class="support-contact"> నవీకరణ జట్టు </a> ఎంపికలు కోసం.';
-$lang['support_day_remaining'] = 'డే మిగిలిన';
+$lang['support_contact_upgrades'] = 'మా సంప్రదించండి <a href="#" class="support-contact"> అప్గ్రేడ్ బృందం </a> ఎంపికలు కోసం.';
+$lang['support_day_remaining'] = 'మిగిలిన రోజు';
 $lang['support_days_remaining'] = 'మిగిలి ఉన్న రోజులు';
 $lang['support_documentation'] = 'డాక్యుమెంటేషన్';
 $lang['support_knowledge_base'] = 'అధునాతన నాలెడ్జ్ బేస్';
 $lang['support_learn_more'] = 'ఇంకా నేర్చుకో';
-$lang['support_no_open_tickets'] = 'ఓపెన్ టిక్కెట్లు లేవు';
+$lang['support_no_open_tickets'] = 'ఓపెన్ టికెట్లు';
 $lang['support_not_available'] = 'అందుబాటులో లేదు';
 $lang['support_phone'] = 'ఫోన్';
-$lang['support_please_open_ticket'] = 'ఒక చాట్లో ప్రారంభించడానికి ముందు ఒక మద్దతు టికెట్ తెరవడానికి దయచేసి.';
-$lang['support_realtime_chat'] = 'రియల్ టైమ్ చాట్ మద్దతు';
-$lang['support_submit_bug_report'] = 'సమర్పించండి ఒక బగ్ను నివేదించండి';
-$lang['support_submit_ticket'] = 'ఒక మద్దతు పత్రాన్ని సమర్పించండి';
-$lang['support_upgrades_and_per_incident'] = 'మద్దతు కోసం అర్హత ఉండాలి, ఒక్కొక్క సంఘటన టికెట్ కొనుగోలు లేదా మీ చందా స్థాయిని అప్గ్రేడ్ చేయండి <a href="#" class="support-upgrade-url"> ఇక్కడ </a> .';
-$lang['support_upgrade'] = 'నవీకరణ';
-$lang['support_upgrade_window'] = 'అర్హత అప్గ్రేడ్';
+$lang['support_please_open_ticket'] = 'దయచేసి చాట్ సెషన్ను ప్రారంభించడానికి ముందు ఒక మద్దతు టికెట్ తెరవండి.';
+$lang['support_realtime_chat'] = 'రియల్ టైమ్ చాట్ మద్దతు';
+$lang['support_submit_bug_report'] = 'బగ్ నివేదికను సమర్పించండి';
+$lang['support_submit_ticket'] = 'ఒక మద్దతు టికెట్ సమర్పించండి';
+$lang['support_upgrades_and_per_incident'] = 'మద్దతు కోసం అర్హత పొందేందుకు, దయచేసి ప్రతి సంఘటన టికెట్ని కొనుగోలు చేయండి లేదా మీ సభ్యత్వ స్థాయిని అప్గ్రేడ్ చేయండి <a href="#" class="support-upgrade-url"> ఇక్కడ </a> .';
+$lang['support_upgrade'] = 'అప్గ్రేడ్';
+$lang['support_upgrade_window'] = 'అర్హతను అప్గ్రేడ్ చేయండి';
 $lang['support_web'] = 'వెబ్';
