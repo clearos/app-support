0bb5f4144c6486043a46c0c7add6f3786273a1d7
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:37:26 +0000

    Add new files and directories, commit changes, push changes

--- language/vi/support_lang.php
+++ language/vi/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Options và nguồn lực để hỗ trợ hệ thống của bạn. Nếu bạn muốn thảo luận về yêu cầu của bạn, xin vui lòng <a href="#" class="support-contact"> liên hệ chúng tôi </a> .';
+$lang['support_app_description'] = 'Tùy chọn và tài nguyên để hỗ trợ hệ thống của bạn. Nếu bạn muốn thảo luận các yêu cầu của bạn, vui lòng <a href="#" class="support-contact"> liên hệ chúng tôi </a> .';
 $lang['support_app_name'] = 'Ủng hộ';
-$lang['support_chat_not_available'] = 'Hỗ trợ qua chat là không có sẵn tại thời điểm này.';
+$lang['support_chat_not_available'] = 'Hỗ trợ qua trò chuyện hiện không có.';
 $lang['support_community_forums'] = 'Diễn đàn cộng đồng';
-$lang['support_contact_upgrades'] = 'Liên lạc với chúng tôi <a href="#" class="support-contact"> đội nâng cấp </a> cho các tùy chọn.';
-$lang['support_day_remaining'] = 'ngày còn lại';
+$lang['support_contact_upgrades'] = 'Liên hệ với chúng tôi <a href="#" class="support-contact"> đội nâng cấp </a> cho các tùy chọn.';
+$lang['support_day_remaining'] = 'Ngày còn lại';
 $lang['support_days_remaining'] = 'những ngày còn lai';
 $lang['support_documentation'] = 'Tài liệu';
-$lang['support_knowledge_base'] = 'Cơ sở kiến ​​thức nâng cao';
+$lang['support_knowledge_base'] = 'Cơ sở Kiến thức Nâng cao';
 $lang['support_learn_more'] = 'Tìm hiểu thêm';
-$lang['support_no_open_tickets'] = 'Không mở Vé';
+$lang['support_no_open_tickets'] = 'Không có vé mở';
 $lang['support_not_available'] = 'Không có sẵn';
 $lang['support_phone'] = 'Điện thoại';
-$lang['support_please_open_ticket'] = 'Hãy mở một vé hỗ trợ trước khi bắt đầu một phiên chat.';
-$lang['support_realtime_chat'] = 'Chat Hỗ trợ thời gian thực';
-$lang['support_submit_bug_report'] = 'Gửi một báo cáo lỗi';
-$lang['support_submit_ticket'] = 'Gửi Ticket Hỗ trợ';
-$lang['support_upgrades_and_per_incident'] = 'Để đủ điều kiện hỗ trợ, hãy mua một vé mỗi sự cố hoặc nâng cấp cấp độ đăng ký của bạn <a href="#" class="support-upgrade-url"> đây </a> .';
-$lang['support_upgrade'] = 'nâng cấp';
-$lang['support_upgrade_window'] = 'nâng cấp đủ điều kiện';
+$lang['support_please_open_ticket'] = 'Vui lòng mở một vé hỗ trợ trước khi bắt đầu một phiên chat.';
+$lang['support_realtime_chat'] = 'Hỗ trợ Trò chuyện Thời gian Thực';
+$lang['support_submit_bug_report'] = 'Gửi báo cáo lỗi';
+$lang['support_submit_ticket'] = 'Gửi Vé Hỗ trợ';
+$lang['support_upgrades_and_per_incident'] = 'Để đủ điều kiện nhận hỗ trợ, vui lòng mua một vé xem mỗi sự kiện hoặc nâng cấp mức thuê bao của bạn <a href="#" class="support-upgrade-url"> đây </a> .';
+$lang['support_upgrade'] = 'Nâng cấp';
+$lang['support_upgrade_window'] = 'Nâng cấp Điều kiện';
 $lang['support_web'] = 'Web';
