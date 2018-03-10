9126ab104295a08b928f81d15301e10e89faebde
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:17:41 +0000

    Add new files and directories, commit changes, push changes

--- language/mr/support_lang.php
+++ language/mr/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'तुमची प्रणाली पाठिंबा पर्याय आणि संसाधने. आपण आपल्या गरजा चर्चा करा इच्छित असल्यास <a href="#" class="support-contact"> आमच्याशी संपर्क </a> .';
+$lang['support_app_description'] = 'तुमची प्रणाली समर्थित करण्यासाठी पर्याय आणि स्रोत. आपण आपल्या आवश्यकता चर्चा करू इच्छित असल्यास, कृपया <a href="#" class="support-contact"> आमच्याशी संपर्क साधा </a> .';
 $lang['support_app_name'] = 'समर्थन';
-$lang['support_chat_not_available'] = 'गप्पा मार्गे समर्थन यावेळी अनुपलब्ध आहे.';
+$lang['support_chat_not_available'] = 'या वेळी गप्पा द्वारे समर्थन अनुपलब्ध आहे';
 $lang['support_community_forums'] = 'समुदाय मंच';
-$lang['support_contact_upgrades'] = 'संपर्क साधा आमच्या <a href="#" class="support-contact"> सुधारणा संघ </a> पहा.';
-$lang['support_day_remaining'] = 'उर्वरित दिवस';
+$lang['support_contact_upgrades'] = 'आमच्याशी संपर्क साधा <a href="#" class="support-contact"> श्रेणी सुधारित करा </a> पर्यायांसाठी';
+$lang['support_day_remaining'] = 'शिल्लक दिवस';
 $lang['support_days_remaining'] = 'उर्वरित दिवस';
 $lang['support_documentation'] = 'दस्तऐवजीकरण';
-$lang['support_knowledge_base'] = 'प्रगत नॉलेज बेस';
+$lang['support_knowledge_base'] = 'प्रगत ज्ञानकोश बेस';
 $lang['support_learn_more'] = 'अधिक जाणून घ्या';
-$lang['support_no_open_tickets'] = 'कोणतेही उघडे तिकीट';
+$lang['support_no_open_tickets'] = 'खुल्या तिकिटे नाहीत';
 $lang['support_not_available'] = 'उपलब्ध नाही';
 $lang['support_phone'] = 'फोन';
-$lang['support_please_open_ticket'] = 'एक चॅट सत्र आरंभ करण्यापूर्वी एक समर्थन तिकीट उघडून करा.';
+$lang['support_please_open_ticket'] = 'चॅट सत्र सुरू करण्यापूर्वी कृपया समर्थन तिकीट उघडा.';
 $lang['support_realtime_chat'] = 'रिअल टाइम गप्पा समर्थन';
-$lang['support_submit_bug_report'] = 'बग अहवाल जमा';
-$lang['support_submit_ticket'] = 'एक समर्थन तिकीट सबमिट';
-$lang['support_upgrades_and_per_incident'] = 'समर्थन करण्यासाठी पात्र, प्रत्येक घटना तिकीट खरेदी किंवा आपल्या सदस्यता पातळी अद्यतनित करा <a href="#" class="support-upgrade-url"> येथे </a> .';
-$lang['support_upgrade'] = 'सुधारणा';
-$lang['support_upgrade_window'] = 'श्रेणीसुधारित करा पात्रता';
+$lang['support_submit_bug_report'] = 'एक बग अहवाल सबमिट करा';
+$lang['support_submit_ticket'] = 'समर्थन तिकीट सबमिट करा';
+$lang['support_upgrades_and_per_incident'] = 'समर्थनासाठी पात्र होण्याकरिता, कृपया प्रति-प्रसंग तिकीट खरेदी करा किंवा आपले सबस्क्रिप्शन स्तर सुधारित करा <a href="#" class="support-upgrade-url"> येथे </a> .';
+$lang['support_upgrade'] = 'श्रेणीसुधारित करा';
+$lang['support_upgrade_window'] = 'पात्रता सुधारित करा';
 $lang['support_web'] = 'वेब';
