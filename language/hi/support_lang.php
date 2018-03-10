7d5ca6bd78696959de722d704743226382b76060
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:03:21 +0000

    Add new files and directories, commit changes, push changes

--- language/hi/support_lang.php
+++ language/hi/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'विकल्प और आपके सिस्टम का समर्थन करने के लिए संसाधनों। आप अपनी आवश्यकताओं के लिए, कृपया पर चर्चा करना चाहते हैं <a href="#" class="support-contact"> हमसे संपर्क करें </a> ।';
+$lang['support_app_description'] = 'आपके सिस्टम का समर्थन करने के लिए विकल्प और संसाधन यदि आप अपनी आवश्यकताओं पर चर्चा करना चाहते हैं, तो कृपया <a href="#" class="support-contact"> हमसे संपर्क करें </a> ।';
 $lang['support_app_name'] = 'समर्थन';
-$lang['support_chat_not_available'] = 'चैट के माध्यम से समर्थन इस समय उपलब्ध नहीं है।';
+$lang['support_chat_not_available'] = 'चैट के माध्यम से सहायता इस समय अनुपलब्ध है।';
 $lang['support_community_forums'] = 'सामुदायिक फ़ोरम्स';
-$lang['support_contact_upgrades'] = 'हमारे संपर्क करें <a href="#" class="support-contact"> उन्नयन टीम </a> विकल्प के लिए।';
-$lang['support_day_remaining'] = 'दिन शेष';
+$lang['support_contact_upgrades'] = 'हमसे संपर्क करें <a href="#" class="support-contact"> उन्नयन टीम </a> विकल्प के लिए';
+$lang['support_day_remaining'] = 'शेष दिन';
 $lang['support_days_remaining'] = 'बचे हुए दिन';
 $lang['support_documentation'] = 'प्रलेखन';
 $lang['support_knowledge_base'] = 'उन्नत ज्ञानकोष';
 $lang['support_learn_more'] = 'और अधिक जानें';
-$lang['support_no_open_tickets'] = 'कोई खुला टिकट';
+$lang['support_no_open_tickets'] = 'कोई ओपन टिकट नहीं';
 $lang['support_not_available'] = 'उपलब्ध नहीं है';
 $lang['support_phone'] = 'फ़ोन';
-$lang['support_please_open_ticket'] = 'कृपया एक बातचीत सत्र की शुरुआत से पहले एक समर्थन टिकट खुला।';
-$lang['support_realtime_chat'] = 'वास्तविक समय चैट समर्थन';
-$lang['support_submit_bug_report'] = 'एक बग रिपोर्ट प्रस्तुत';
-$lang['support_submit_ticket'] = 'जमा एक समर्थन टिकट';
-$lang['support_upgrades_and_per_incident'] = 'समर्थन के लिए पात्र हो, एक प्रति घटना टिकट खरीद या अपनी सदस्यता के स्तर का उन्नयन कृपया <a href="#" class="support-upgrade-url"> यहाँ </a> ।';
-$lang['support_upgrade'] = 'नवीनीकरण';
-$lang['support_upgrade_window'] = 'के उन्नयन पात्रता';
+$lang['support_please_open_ticket'] = 'चैट सत्र की शुरुआत करने से पहले कृपया एक समर्थन टिकट खोलें।';
+$lang['support_realtime_chat'] = 'वास्तविक समय चैट सहायता';
+$lang['support_submit_bug_report'] = 'एक बग रिपोर्ट सबमिट करें';
+$lang['support_submit_ticket'] = 'एक समर्थन टिकट सबमिट करें';
+$lang['support_upgrades_and_per_incident'] = 'समर्थन के लिए योग्य होने के लिए, कृपया प्रति-घटना टिकट खरीदें या अपना सदस्यता स्तर अपग्रेड करें <a href="#" class="support-upgrade-url"> यहाँ </a> ।';
+$lang['support_upgrade'] = 'अपग्रेड';
+$lang['support_upgrade_window'] = 'योग्यता का नवीनीकरण करें';
 $lang['support_web'] = 'वेब';
