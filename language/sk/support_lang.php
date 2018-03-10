417839b287157db01b8d5bf094784121110655fe
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:26:25 +0000

    Add new files and directories, commit changes, push changes

--- language/sk/support_lang.php
+++ language/sk/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Možnosti a prostriedky na podporu vášho systému. Ak by ste chceli diskutovať o vaše požiadavky, prosím <a href="#" class="support-contact"> kontaktuj nás </a> ,';
+$lang['support_app_description'] = 'Možnosti a zdroje na podporu vášho systému. Ak by ste chceli prediskutovať vaše požiadavky, prosím <a href="#" class="support-contact"> kontaktuj nás </a> ,';
 $lang['support_app_name'] = 'podpora';
-$lang['support_chat_not_available'] = 'Podpora prostredníctvom chatu je k dispozícii v tomto okamihu.';
-$lang['support_community_forums'] = 'Fórum Komunita';
-$lang['support_contact_upgrades'] = 'contact <a href="#" class="support-contact"> upgrade tím </a> možnosti.';
-$lang['support_day_remaining'] = 'deň Zostávajúce';
+$lang['support_chat_not_available'] = 'Podpora prostredníctvom rozhovoru nie je momentálne k dispozícii.';
+$lang['support_community_forums'] = 'Fóra komunity';
+$lang['support_contact_upgrades'] = 'Kontaktujte nás <a href="#" class="support-contact"> upgrade tím </a> pre možnosti.';
+$lang['support_day_remaining'] = 'Zostávajúci deň';
 $lang['support_days_remaining'] = 'Dni do konca';
 $lang['support_documentation'] = 'dokumentácia';
-$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
+$lang['support_knowledge_base'] = 'Pokročilá znalostná báza';
 $lang['support_learn_more'] = 'Uč sa viac';
-$lang['support_no_open_tickets'] = 'Žiadne Otvorené Vstupenky';
-$lang['support_not_available'] = 'nie je k dispozícii';
+$lang['support_no_open_tickets'] = 'Žiadne otvorené vstupenky';
+$lang['support_not_available'] = 'Nie je k dispozícií';
 $lang['support_phone'] = 'telefón';
-$lang['support_please_open_ticket'] = 'Prosím, otvorte lístok pomoci pred začatím chatu.';
-$lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Pošlite Bug Report';
-$lang['support_submit_ticket'] = 'Vložte Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'Ak chcete mať nárok na podporu, nutné zakúpiť lístok za udalosť alebo upgrade vašej úrovne odberu <a href="#" class="support-upgrade-url"> tu </a> ,';
+$lang['support_please_open_ticket'] = 'Pred spustením relácie rozhovoru otvorte lístok na podporu.';
+$lang['support_realtime_chat'] = 'Podpora chatu v reálnom čase';
+$lang['support_submit_bug_report'] = 'Odošlite chybovú správu';
+$lang['support_submit_ticket'] = 'Odoslať lístok na podporu';
+$lang['support_upgrades_and_per_incident'] = 'Ak chcete mať nárok na podporu, zakúpte si letenku za prípad alebo aktualizujte svoju úroveň odberu <a href="#" class="support-upgrade-url"> tu </a> ,';
 $lang['support_upgrade'] = 'upgrade';
-$lang['support_upgrade_window'] = 'možnosť upgradu';
+$lang['support_upgrade_window'] = 'Oprávnenie na inováciu';
 $lang['support_web'] = 'web';
