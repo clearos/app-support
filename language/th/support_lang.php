04c959bd6996ee087c49ed5003695ce9bdee2b5e
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:33:30 +0000

    Add new files and directories, commit changes, push changes

--- language/th/support_lang.php
+++ language/th/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'ตัวเลือกและทรัพยากรสำหรับการสนับสนุนระบบของคุณ หากคุณต้องการที่จะหารือเกี่ยวกับความต้องการของคุณโปรด <a href="#" class="support-contact"> ติดต่อเรา </a> .';
+$lang['support_app_description'] = 'ตัวเลือกและทรัพยากรสำหรับสนับสนุนระบบของคุณ หากคุณต้องการพูดคุยเกี่ยวกับความต้องการของคุณโปรด <a href="#" class="support-contact"> ติดต่อเรา </a> .';
 $lang['support_app_name'] = 'สนับสนุน';
-$lang['support_chat_not_available'] = 'การสนับสนุนผ่านการแชทไม่พร้อมใช้งานในขณะนี้';
+$lang['support_chat_not_available'] = 'การสนับสนุนทางแชทไม่สามารถใช้งานได้ในขณะนี้';
 $lang['support_community_forums'] = 'ฟอรัมชุมชน';
-$lang['support_contact_upgrades'] = 'การติดต่อของเรา <a href="#" class="support-contact"> ทีมอัพเกรด </a> สำหรับตัวเลือก';
+$lang['support_contact_upgrades'] = 'ติดต่อเรา <a href="#" class="support-contact"> ทีมอัพเกรด </a> สำหรับตัวเลือก';
 $lang['support_day_remaining'] = 'วันที่เหลืออยู่';
 $lang['support_days_remaining'] = 'วันเวลาที่เหลือ';
 $lang['support_documentation'] = 'เอกสาร';
 $lang['support_knowledge_base'] = 'ฐานความรู้ขั้นสูง';
 $lang['support_learn_more'] = 'เรียนรู้เพิ่มเติม';
-$lang['support_no_open_tickets'] = 'ไม่มีเปิดตั๋ว';
-$lang['support_not_available'] = 'ไม่สามารถใช้งาน';
+$lang['support_no_open_tickets'] = 'ไม่มีตั๋วเปิด';
+$lang['support_not_available'] = 'ไม่พร้อมใช้งาน';
 $lang['support_phone'] = 'โทรศัพท์';
-$lang['support_please_open_ticket'] = 'โปรดเปิดตั๋วสนับสนุนเซสชั่นก่อนที่จะเริ่มการสนทนา';
-$lang['support_realtime_chat'] = 'สนับสนุนการพูดคุยแบบ Real Time';
-$lang['support_submit_bug_report'] = 'ส่งรายงานข้อผิดพลาด';
-$lang['support_submit_ticket'] = 'ส่งตั๋วสนับสนุน';
-$lang['support_upgrades_and_per_incident'] = 'จึงจะมีสิทธิ์สำหรับการสนับสนุนโปรดซื้อตั๋วต่อเหตุการณ์ที่เกิดขึ้นหรืออัพเกรดระดับการสมัครของคุณ <a href="#" class="support-upgrade-url"> ที่นี่ </a> .';
+$lang['support_please_open_ticket'] = 'โปรดเปิดบัตรสนับสนุนก่อนที่จะเริ่มเซสชั่นแชท';
+$lang['support_realtime_chat'] = 'การสนับสนุนแบบเรียลไทม์แชท';
+$lang['support_submit_bug_report'] = 'ส่งรายงานข้อบกพร่อง';
+$lang['support_submit_ticket'] = 'ส่งบัตรการสนับสนุน';
+$lang['support_upgrades_and_per_incident'] = 'หากต้องการมีสิทธิ์รับการสนับสนุนโปรดซื้อตั๋วต่อเหตุการณ์หรืออัปเกรดระดับการสมัครรับข้อมูลของคุณ <a href="#" class="support-upgrade-url"> ที่นี่ </a> .';
 $lang['support_upgrade'] = 'อัพเกรด';
-$lang['support_upgrade_window'] = 'อัพเกรดมีสิทธิ์';
+$lang['support_upgrade_window'] = 'อัปเกรดสิทธิ์';
 $lang['support_web'] = 'เว็บ';
