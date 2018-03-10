3685d3a19083698e0b0830199e7cc0c2a53fc486
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 09:47:08 +0000

    Add new files and directories, commit changes, push changes

--- language/ar/support_lang.php
+++ language/ar/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'الخيارات والموارد لدعم النظام الخاص بك. إذا كنت ترغب في مناقشة الاحتياجات الخاصة بك، يرجى <a href="#" class="support-contact"> اتصل بنا </a> .';
-$lang['support_app_name'] = 'الدعم';
-$lang['support_chat_not_available'] = 'الدعم عبر الدردشة غير متوفرة في هذا الوقت.';
+$lang['support_app_description'] = 'خيارات وموارد لدعم النظام الخاص بك. إذا كنت ترغب في مناقشة متطلباتك ، من فضلك <a href="#" class="support-contact"> اتصل بنا </a> .';
+$lang['support_app_name'] = 'پشتیبانی';
+$lang['support_chat_not_available'] = 'الدعم عبر الدردشة غير متاح في هذا الوقت.';
 $lang['support_community_forums'] = 'منتديات إجتماعية';
-$lang['support_contact_upgrades'] = 'اتصل بنا <a href="#" class="support-contact"> فريق الترقية </a> للحصول على خيارات.';
-$lang['support_day_remaining'] = 'يوم المتبقية';
+$lang['support_contact_upgrades'] = 'اتصل بنا <a href="#" class="support-contact"> فريق الترقية </a> للخيارات.';
+$lang['support_day_remaining'] = 'اليوم المتبقي';
 $lang['support_days_remaining'] = 'الأيام المتبقية';
 $lang['support_documentation'] = 'كابل بيانات';
-$lang['support_knowledge_base'] = 'قاعدة المعارف المتقدم';
+$lang['support_knowledge_base'] = 'قاعدة المعرفة المتقدمة';
 $lang['support_learn_more'] = 'أعرف أكثر';
-$lang['support_no_open_tickets'] = 'لا تذاكر المفتوحة';
+$lang['support_no_open_tickets'] = 'لا التذاكر المفتوحة';
 $lang['support_not_available'] = 'غير متاح';
 $lang['support_phone'] = 'هاتف';
-$lang['support_please_open_ticket'] = 'يرجى فتح تذكرة دعم قبل بدء جلسة دردشة.';
-$lang['support_realtime_chat'] = 'الدردشة في الوقت الحقيقي دعم';
-$lang['support_submit_bug_report'] = 'تقديم تقرير الشوائب';
-$lang['support_submit_ticket'] = 'إرسال تذكرة دعم';
-$lang['support_upgrades_and_per_incident'] = 'ليكون مؤهلا للحصول على الدعم، يرجى شراء تذكرة لكل حادث أو ترقية مستوى اشتراكك <a href="#" class="support-upgrade-url"> هنا </a> .';
+$lang['support_please_open_ticket'] = 'يرجى فتح بطاقة دعم قبل بدء جلسة محادثة.';
+$lang['support_realtime_chat'] = 'دعم الدردشة في الوقت الحقيقي';
+$lang['support_submit_bug_report'] = 'أرسل تقريرًا عن الخطأ';
+$lang['support_submit_ticket'] = 'تقديم تذكرة الدعم';
+$lang['support_upgrades_and_per_incident'] = 'لكي تكون مؤهلاً للحصول على الدعم ، يرجى شراء تذكرة لكل حادث أو ترقية مستوى الاشتراك الخاص بك <a href="#" class="support-upgrade-url"> هنا </a> .';
 $lang['support_upgrade'] = 'تطوير';
 $lang['support_upgrade_window'] = 'ترقية الأهلية';
 $lang['support_web'] = 'شبكة';
