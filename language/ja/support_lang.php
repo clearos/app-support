d8fb5042393a02414f8863f876a27a0e26a5f30d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:07:55 +0000

    Add new files and directories, commit changes, push changes

--- language/ja/support_lang.php
+++ language/ja/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'お使いのシステムをサポートするためのオプションとリソース。 あなたは、あなたの要件をしてください議論したい場合<a href="#" class="support-contact">お問い合わせ</a> 。';
+$lang['support_app_description'] = 'システムをサポートするためのオプションとリソース。 あなたの要件を議論したい場合は、 <a href="#" class="support-contact">お問い合わせ</a> 。';
 $lang['support_app_name'] = 'サポート';
-$lang['support_chat_not_available'] = 'チャットでのサポートは現時点では利用できません。';
+$lang['support_chat_not_available'] = '現時点では、チャットによるサポートはご利用いただけません。';
 $lang['support_community_forums'] = 'コミュニティフォーラム';
-$lang['support_contact_upgrades'] = '私たちにお問い合わせください<a href="#" class="support-contact">アップグレードチーム</a>オプションについて。';
-$lang['support_day_remaining'] = '残り日';
+$lang['support_contact_upgrades'] = 'お問い合わせ<a href="#" class="support-contact">アップグレードチーム</a>オプションのために。';
+$lang['support_day_remaining'] = '残りの日';
 $lang['support_days_remaining'] = '残存日数';
 $lang['support_documentation'] = 'ドキュメンテーション';
-$lang['support_knowledge_base'] = '高度なナレッジベース';
+$lang['support_knowledge_base'] = '高度な知識ベース';
 $lang['support_learn_more'] = 'もっと詳しく知る';
-$lang['support_no_open_tickets'] = 'いいえオープンチケットはありません';
+$lang['support_no_open_tickets'] = 'オープンチケットなし';
 $lang['support_not_available'] = '利用不可';
 $lang['support_phone'] = '電話';
-$lang['support_please_open_ticket'] = 'チャットセッションを開始する前に、サポートチケットを開いてください。';
-$lang['support_realtime_chat'] = 'リアルタイムチャットサポート';
-$lang['support_submit_bug_report'] = 'バグレポートを提出';
-$lang['support_submit_ticket'] = 'サポートチケットを送信';
-$lang['support_upgrades_and_per_incident'] = 'サポートの対象となるためには、単位のインシデントチケットを購入するか、サブスクリプションレベルをアップグレードしてください<a href="#" class="support-upgrade-url">ここに</a> 。';
+$lang['support_please_open_ticket'] = 'チャットセッションを開始する前にサポートチケットを開いてください。';
+$lang['support_realtime_chat'] = 'リアルタイムチャットのサポート';
+$lang['support_submit_bug_report'] = 'バグレポートを送信する';
+$lang['support_submit_ticket'] = 'サポートチケットを送信する';
+$lang['support_upgrades_and_per_incident'] = 'サポートを受けるには、インシデントごとのチケットを購入するか、購読レベルをアップグレードしてください<a href="#" class="support-upgrade-url">ここに</a> 。';
 $lang['support_upgrade'] = 'アップグレード';
-$lang['support_upgrade_window'] = '資格をアップグレード';
+$lang['support_upgrade_window'] = 'アップグレードの適格性';
 $lang['support_web'] = 'ウェブ';
