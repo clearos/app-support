905897b1c6a78bced437389b8f318d7379cf58a8
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:04:35 +0000

    Add new files and directories, commit changes, push changes

--- language/id/support_lang.php
+++ language/id/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Pilihan dan sumber daya untuk mendukung sistem Anda. Jika Anda ingin mendiskusikan kebutuhan Anda, silakan <a href="#" class="support-contact"> hubungi kami </a> .';
+$lang['support_app_description'] = 'Pilihan dan sumber daya untuk mendukung sistem anda. Jika Anda ingin mendiskusikan kebutuhan Anda, silakan <a href="#" class="support-contact"> hubungi kami </a> .';
 $lang['support_app_name'] = 'Mendukung';
-$lang['support_chat_not_available'] = 'Dukungan melalui chat tidak tersedia pada saat ini.';
-$lang['support_community_forums'] = 'Forum komunitas';
-$lang['support_contact_upgrades'] = 'hubungi kami <a href="#" class="support-contact"> tim Upgrade </a> untuk pilihan.';
-$lang['support_day_remaining'] = 'hari tersisa';
+$lang['support_chat_not_available'] = 'Dukungan via chat tidak tersedia saat ini.';
+$lang['support_community_forums'] = 'Forum Komunitas';
+$lang['support_contact_upgrades'] = 'Hubungi kami <a href="#" class="support-contact"> tim upgrade </a> untuk pilihan';
+$lang['support_day_remaining'] = 'Sisa hari';
 $lang['support_days_remaining'] = 'Hari Tersisa';
 $lang['support_documentation'] = 'Dokumentasi';
-$lang['support_knowledge_base'] = 'Basis Pengetahuan canggih';
+$lang['support_knowledge_base'] = 'Basis Pengetahuan Lanjutan';
 $lang['support_learn_more'] = 'Belajarlah lagi';
-$lang['support_no_open_tickets'] = 'Tidak Buka Tiket';
+$lang['support_no_open_tickets'] = 'Tidak ada Tiket Terbuka';
 $lang['support_not_available'] = 'Tidak tersedia';
 $lang['support_phone'] = 'Telepon';
-$lang['support_please_open_ticket'] = 'Silahkan buka tiket dukungan sebelum memulai sesi chat.';
-$lang['support_realtime_chat'] = 'Real Time Chat Dukungan';
-$lang['support_submit_bug_report'] = 'Kirim Laporan Bug';
-$lang['support_submit_ticket'] = 'Kirim Tiket Support';
-$lang['support_upgrades_and_per_incident'] = 'Agar memenuhi syarat untuk dukungan, silakan membeli tiket per-insiden atau meng-upgrade tingkat langganan Anda <a href="#" class="support-upgrade-url"> sini </a> .';
+$lang['support_please_open_ticket'] = 'Silakan buka tiket dukungan sebelum memulai sesi obrolan.';
+$lang['support_realtime_chat'] = 'Dukungan Waktu Nyata';
+$lang['support_submit_bug_report'] = 'Kirimkan Laporan Bug';
+$lang['support_submit_ticket'] = 'Kirimkan Tiket Dukungan';
+$lang['support_upgrades_and_per_incident'] = 'Agar memenuhi syarat untuk mendapatkan dukungan, silakan membeli tiket dengan biaya per kejadian atau meningkatkan tingkat langganan Anda <a href="#" class="support-upgrade-url"> sini </a> .';
 $lang['support_upgrade'] = 'Meningkatkan';
 $lang['support_upgrade_window'] = 'Upgrade Kelayakan';
-$lang['support_web'] = 'jaringan';
+$lang['support_web'] = 'Web';
