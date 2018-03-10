c8e3e0668df92286ac1b02a72d392b1bad208b4b
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:01:07 +0000

    Add new files and directories, commit changes, push changes

--- language/gu/support_lang.php
+++ language/gu/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'વિકલ્પો અને તમારી સિસ્ટમ સહાયક માટે સંસાધનો. તમે તમારી જરૂરિયાતો માટે, કૃપા કરીને ચર્ચા કરવા માંગો છો, તો <a href="#" class="support-contact"> અમારો સંપર્ક કરો </a> .';
+$lang['support_app_description'] = 'તમારી સિસ્ટમ સહાય માટે વિકલ્પો અને સ્રોતો. જો તમે તમારી જરૂરિયાતો અંગે ચર્ચા કરવા માગો છો, તો કૃપા કરીને <a href="#" class="support-contact"> અમારો સંપર્ક કરો </a> .';
 $lang['support_app_name'] = 'આધાર';
-$lang['support_chat_not_available'] = 'ચેટ મારફતે આધાર આ સમયે અનુપલબ્ધ છે.';
+$lang['support_chat_not_available'] = 'ચેટ દ્વારા સમર્થન આ સમયે અનુપલબ્ધ છે.';
 $lang['support_community_forums'] = 'કોમ્યુનિટી ફોરમ્સ';
-$lang['support_contact_upgrades'] = 'સંપર્ક અમારા <a href="#" class="support-contact"> સુધારો ટીમ </a> વિકલ્પો છે.';
-$lang['support_day_remaining'] = 'દિવસ બાકી';
+$lang['support_contact_upgrades'] = 'અમારો સંપર્ક કરો <a href="#" class="support-contact"> સુધારો ટીમ </a> વિકલ્પો માટે';
+$lang['support_day_remaining'] = 'બાકીના દિવસ';
 $lang['support_days_remaining'] = 'બાકી દિવસો';
 $lang['support_documentation'] = 'દસ્તાવેજીકરણ';
 $lang['support_knowledge_base'] = 'ઉન્નત નોલેજ બેઝ';
 $lang['support_learn_more'] = 'વધુ શીખો';
-$lang['support_no_open_tickets'] = 'કોઈ ઓપન ટિકિટ';
+$lang['support_no_open_tickets'] = 'કોઈ ઓપન ટિકિટ નથી';
 $lang['support_not_available'] = 'ઉપલબ્ધ નથી';
 $lang['support_phone'] = 'ફોન';
-$lang['support_please_open_ticket'] = 'કૃપા કરીને ચેટ સત્ર શરૂ કરો તે પહેલાં એક આધાર ટિકિટ ખોલવા.';
-$lang['support_realtime_chat'] = 'પ્રત્યક્ષ સમયનો ચેટ આધાર';
-$lang['support_submit_bug_report'] = 'ક્ષતિ અહેવાલ સબમિટ';
-$lang['support_submit_ticket'] = 'એક આધાર ટિકિટ સબમિટ';
-$lang['support_upgrades_and_per_incident'] = 'આધાર માટે પાત્ર થવા માટે, પ્રતિ ઘટના ટિકિટ ખરીદી અથવા તમારા ઉમેદવારી સ્તર અપગ્રેડ કરો <a href="#" class="support-upgrade-url"> અહીં </a> .';
-$lang['support_upgrade'] = 'સુધારો';
-$lang['support_upgrade_window'] = 'સુધારો લાયકાત';
+$lang['support_please_open_ticket'] = 'ચૅટ સત્રની શરૂઆત કરતા પહેલા સપોર્ટ ટિકિટ ખોલો.';
+$lang['support_realtime_chat'] = 'પ્રત્યક્ષ સમયનો ચેટ સપોર્ટ';
+$lang['support_submit_bug_report'] = 'બગ રિપોર્ટ સબમિટ કરો';
+$lang['support_submit_ticket'] = 'સપોર્ટ ટિકિટ સબમિટ કરો';
+$lang['support_upgrades_and_per_incident'] = 'સમર્થન માટે લાયક બનવા માટે, કૃપા કરીને દીઠ-ઘટના ટિકિટ ખરીદી અથવા તમારા સબ્સ્ક્રિપ્શન સ્તરને અપગ્રેડ કરો <a href="#" class="support-upgrade-url"> અહીં </a> .';
+$lang['support_upgrade'] = 'અપગ્રેડ કરો';
+$lang['support_upgrade_window'] = 'પાત્રતાને અપગ્રેડ કરો';
 $lang['support_web'] = 'વેબ';
