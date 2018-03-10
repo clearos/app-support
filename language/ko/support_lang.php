9aa331d70d784c7a40077f6d35eb706d9ce1a868
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:11:12 +0000

    Add new files and directories, commit changes, push changes

--- language/ko/support_lang.php
+++ language/ko/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = '시스템을 지원하기위한 옵션 및 자원. 당신이 당신의 요구 사항을하시기 바랍니다 논의하려는 경우 <a href="#" class="support-contact"> 문의 </a> .';
+$lang['support_app_description'] = '시스템을 지원하기위한 옵션 및 자원. 요구 사항에 대해 논의하고 싶다면 <a href="#" class="support-contact"> 문의하기 </a> .';
 $lang['support_app_name'] = '지원하다';
-$lang['support_chat_not_available'] = '채팅을 통해 지원은 현재 사용할 수 없습니다.';
+$lang['support_chat_not_available'] = '현재 채팅을 통한 지원을 이용할 수 없습니다.';
 $lang['support_community_forums'] = '커뮤니티 포럼';
-$lang['support_contact_upgrades'] = '연락 우리 <a href="#" class="support-contact"> 업그레이드 팀 </a> 옵션.';
+$lang['support_contact_upgrades'] = '문의하기 <a href="#" class="support-contact"> 팀 업그레이드 </a> 옵션.';
 $lang['support_day_remaining'] = '남은 날';
 $lang['support_days_remaining'] = '남은 일';
 $lang['support_documentation'] = '선적 서류 비치';
 $lang['support_knowledge_base'] = '고급 기술 자료';
-$lang['support_learn_more'] = '자세히 알아보기';
-$lang['support_no_open_tickets'] = '열린 티켓 없습니다';
+$lang['support_learn_more'] = '더 알아보기';
+$lang['support_no_open_tickets'] = '열리는 티켓 없음';
 $lang['support_not_available'] = '사용 불가';
 $lang['support_phone'] = '전화';
 $lang['support_please_open_ticket'] = '채팅 세션을 시작하기 전에 지원 티켓을여십시오.';
 $lang['support_realtime_chat'] = '실시간 채팅 지원';
-$lang['support_submit_bug_report'] = '버그 리포트를 제출';
-$lang['support_submit_ticket'] = '기술 지원 티켓을 제출';
-$lang['support_upgrades_and_per_incident'] = '지원 자격을 얻으려면, 당 사건 티켓을 구입하거나 구독 레벨을 업그레이드하세요 <a href="#" class="support-upgrade-url"> 이리 </a> .';
+$lang['support_submit_bug_report'] = '버그 보고서 제출';
+$lang['support_submit_ticket'] = '지원 티켓 제출';
+$lang['support_upgrades_and_per_incident'] = '지원을 받으려면 인시던트 티켓을 구입하거나 가입 레벨을 업그레이드하십시오. <a href="#" class="support-upgrade-url"> 이리 </a> .';
 $lang['support_upgrade'] = '업그레이드';
-$lang['support_upgrade_window'] = '자격 업그레이드';
+$lang['support_upgrade_window'] = '업그레이드 자격';
 $lang['support_web'] = '편물';
