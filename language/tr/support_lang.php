64ed93ef4241a3069422e961f28922ce79802123
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:34:52 +0000

    Add new files and directories, commit changes, push changes

--- language/tr/support_lang.php
+++ language/tr/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Sisteminizi desteklemek için seçenekler ve kaynaklar. Eğer, ihtiyaçlarınızı lütfen görüşmek isterseniz <a href="#" class="support-contact"> bizimle iletişime geçin </a> .';
+$lang['support_app_description'] = 'Sisteminizi destekleyen seçenekler ve kaynaklar. Gereksinimlerinizi tartışmak isterseniz lütfen <a href="#" class="support-contact"> bizimle iletişime geçin </a> .';
 $lang['support_app_name'] = 'Destek';
-$lang['support_chat_not_available'] = 'sohbet yoluyla destek şu anda kullanılamıyor.';
+$lang['support_chat_not_available'] = 'Sohbet yoluyla destek şu anda kullanılamıyor.';
 $lang['support_community_forums'] = 'topluluk forumları';
-$lang['support_contact_upgrades'] = 'kurun bizim <a href="#" class="support-contact"> yükseltme takım </a> seçenekleri için.';
+$lang['support_contact_upgrades'] = 'Iletişim <a href="#" class="support-contact"> takımı yükselt </a> seçenekler için.';
 $lang['support_day_remaining'] = 'Kalan gün';
 $lang['support_days_remaining'] = 'kalan günler';
 $lang['support_documentation'] = 'belgeleme';
-$lang['support_knowledge_base'] = 'Gelişmiş Bilgi Bankası';
+$lang['support_knowledge_base'] = 'Gelişmiş Bilgi Tabanı';
 $lang['support_learn_more'] = 'Daha fazla bilgi edin';
-$lang['support_no_open_tickets'] = 'Hiçbir Open Biletleri';
+$lang['support_no_open_tickets'] = 'Açık Bilet Yok';
 $lang['support_not_available'] = 'Müsait değil';
 $lang['support_phone'] = 'Telefon';
-$lang['support_please_open_ticket'] = 'Bir sohbet oturumu başlamadan önce bir destek bileti açın.';
+$lang['support_please_open_ticket'] = 'Lütfen bir sohbet oturumu başlatmadan önce bir destek bileti açın.';
 $lang['support_realtime_chat'] = 'Gerçek Zamanlı Sohbet Desteği';
-$lang['support_submit_bug_report'] = 'Bir Hata Raporu Gönder';
-$lang['support_submit_ticket'] = 'Destek Bildirim Gönder';
-$lang['support_upgrades_and_per_incident'] = 'destek için uygun olabilmesi için, bir olay başına bilet satın veya abonelik düzeyini yükseltmek lütfen <a href="#" class="support-upgrade-url"> İşte </a> .';
-$lang['support_upgrade'] = 'yükseltmek';
-$lang['support_upgrade_window'] = 'Uygunluk yükseltme';
+$lang['support_submit_bug_report'] = 'Hata Raporu Gönder';
+$lang['support_submit_ticket'] = 'Destek Bileti Gönderin';
+$lang['support_upgrades_and_per_incident'] = 'Destek almaya uygun olmak için lütfen olay başına bir bilet satın alın veya abonelik seviyenizi yükseltin <a href="#" class="support-upgrade-url"> İşte </a> .';
+$lang['support_upgrade'] = 'Yükselt';
+$lang['support_upgrade_window'] = 'Yükseltme Uygunluğu';
 $lang['support_web'] = 'ağ';
