c3b502ce620b640070b09bf761000983ac03ad82
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:11:39 +0000

    Add new files and directories, commit changes, push changes

--- language/la/support_lang.php
+++ language/la/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Bene, et pro opibus vestris sustinendo ratio. Si agere velit metus, commodo <a href="#" class="support-contact"> nobis loquere </a> .';
+$lang['support_app_description'] = 'Options et pro opibus vestris sustinendo ratio. Si vestri &#39;amo ut de obligatione, obsecro, <a href="#" class="support-contact"> nobis loquere </a> .';
 $lang['support_app_name'] = 'Support';
-$lang['support_chat_not_available'] = 'Support via chat hac perpendat.';
-$lang['support_community_forums'] = 'Community Forums';
-$lang['support_contact_upgrades'] = 'Contact nostris <a href="#" class="support-contact"> upgrade ipsum </a> bene est.';
-$lang['support_day_remaining'] = 'diei superest';
+$lang['support_chat_not_available'] = 'Est via chat auxilium unavailable ad hoc tempus.';
+$lang['support_community_forums'] = 'Forum';
+$lang['support_contact_upgrades'] = 'contact nos <a href="#" class="support-contact"> bigas upgrade </a> ad optiones.';
+$lang['support_day_remaining'] = 'Reliquis diem';
 $lang['support_days_remaining'] = 'Reliquis diebus';
-$lang['support_documentation'] = 'Documentation';
-$lang['support_knowledge_base'] = 'Advanced scientia Base';
+$lang['support_documentation'] = 'documenta';
+$lang['support_knowledge_base'] = 'Provectus scientia Base';
 $lang['support_learn_more'] = 'Discere plus';
-$lang['support_no_open_tickets'] = 'Nullam manifesta';
-$lang['support_not_available'] = 'Not Available';
+$lang['support_no_open_tickets'] = 'Non Open Tickets';
+$lang['support_not_available'] = 'not Available';
 $lang['support_phone'] = 'Phone';
-$lang['support_please_open_ticket'] = 'Aperi tessera firmamentum Curabitur a ante sessionem indicere.';
-$lang['support_realtime_chat'] = 'Verus Vicis Support Chat';
+$lang['support_please_open_ticket'] = 'Placere auxilium aperire tessera ad chat ante sessionem indicere.';
+$lang['support_realtime_chat'] = 'Verus tempus Support Chat';
 $lang['support_submit_bug_report'] = 'Submit Report a Bug';
-$lang['support_submit_ticket'] = 'Submit a Aliquam Services';
-$lang['support_upgrades_and_per_incident'] = 'Ad esse eligibile pro auxilio, placet emere tessera per accidens, vel upgrade tua gradu Subscription <a href="#" class="support-upgrade-url"> hic </a> .';
+$lang['support_submit_ticket'] = 'Aliquam sit submittere Support';
+$lang['support_upgrades_and_per_incident'] = 'Firmamentum esse eligibile placet emere tessera conjunctas vel per planum upgrade subscriptione <a href="#" class="support-upgrade-url"> hic </a> .';
 $lang['support_upgrade'] = 'Upgrade';
 $lang['support_upgrade_window'] = 'Upgrade Eligibility';
 $lang['support_web'] = 'Web';
