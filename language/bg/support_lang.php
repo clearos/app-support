7724f90b2fef8415a3690e00f332ee9adb087823
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 09:51:47 +0000

    Add new files and directories, commit changes, push changes

--- language/bg/support_lang.php
+++ language/bg/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Опции и ресурси за подкрепа на вашата система. Ако бихте искали да обсъдим вашите изисквания, моля <a href="#" class="support-contact"> свържете се с нас </a> ,';
+$lang['support_app_description'] = 'Опции и ресурси за поддръжка на вашата система. Ако искате да обсъдите вашите изисквания, моля <a href="#" class="support-contact"> свържете се с нас </a> ,';
 $lang['support_app_name'] = 'поддържа';
-$lang['support_chat_not_available'] = 'Подкрепа чрез чат е налице в момента.';
-$lang['support_community_forums'] = 'Форуми на Общността';
-$lang['support_contact_upgrades'] = 'Свържете се с нашия <a href="#" class="support-contact"> ъпгрейд отбор </a> за опции.';
-$lang['support_day_remaining'] = 'Ден Оставащ';
+$lang['support_chat_not_available'] = 'Понастоящем не е налице поддръжка чрез чат.';
+$lang['support_community_forums'] = 'Общностни форуми';
+$lang['support_contact_upgrades'] = 'Свържете се с нас <a href="#" class="support-contact"> екип за надграждане </a> за опции.';
+$lang['support_day_remaining'] = 'Оставащ ден';
 $lang['support_days_remaining'] = 'Оставащи дни';
 $lang['support_documentation'] = 'документация';
-$lang['support_knowledge_base'] = 'Base Advanced Knowledge';
+$lang['support_knowledge_base'] = 'Подробна база за знания';
 $lang['support_learn_more'] = 'Научете повече';
-$lang['support_no_open_tickets'] = 'Не Отворени бюлетини';
+$lang['support_no_open_tickets'] = 'Няма отворени билети';
 $lang['support_not_available'] = 'Не е наличен';
 $lang['support_phone'] = 'телефон';
-$lang['support_please_open_ticket'] = 'Моля, отворете билет подкрепа преди започване на чат сесия.';
-$lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Подаване на Уведомление за грешка';
-$lang['support_submit_ticket'] = 'Изпратете Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'За да бъдат допустими за подпомагане, моля закупят билет на инцидент или ъпгрейд нивото на абонамента <a href="#" class="support-upgrade-url"> тук </a> ,';
+$lang['support_please_open_ticket'] = 'Моля, отворете билет за поддръжка, преди да започнете сесия за чат.';
+$lang['support_realtime_chat'] = 'Поддръжка на чат в реално време';
+$lang['support_submit_bug_report'] = 'Изпратете доклад за грешки';
+$lang['support_submit_ticket'] = 'Подайте билет за поддръжка';
+$lang['support_upgrades_and_per_incident'] = 'За да отговаряте на условията за поддръжка, моля, закупете билет за всеки инцидент или надстройте нивото на абонамента си <a href="#" class="support-upgrade-url"> тук </a> ,';
 $lang['support_upgrade'] = 'Подобряване на';
-$lang['support_upgrade_window'] = 'надграждане';
+$lang['support_upgrade_window'] = 'Възможност за надстройване';
 $lang['support_web'] = 'мрежа';
