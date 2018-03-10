9cdd92b8ad94f298eba99eda1985a2b80ebf1624
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:20:10 +0000

    Add new files and directories, commit changes, push changes

--- language/nl/support_lang.php
+++ language/nl/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Opties en middelen voor de ondersteuning van uw systeem. Als u graag om uw wensen te bespreken, neem dan <a href="#" class="support-contact"> Neem contact met ons op </a> .';
+$lang['support_app_description'] = 'Opties en bronnen voor ondersteuning van uw systeem. Als u uw wensen wilt bespreken, alstublieft <a href="#" class="support-contact"> Neem contact met ons op </a> .';
 $lang['support_app_name'] = 'Ondersteuning';
-$lang['support_chat_not_available'] = 'Ondersteuning via chat is niet beschikbaar op dit moment.';
+$lang['support_chat_not_available'] = 'Ondersteuning via chat is op dit moment niet beschikbaar.';
 $lang['support_community_forums'] = 'gemeenschapsforums';
-$lang['support_contact_upgrades'] = 'Neem contact op met ons <a href="#" class="support-contact"> upgrade team </a> voor opties.';
-$lang['support_day_remaining'] = 'dag van de Rest';
+$lang['support_contact_upgrades'] = 'Neem contact op met onze <a href="#" class="support-contact"> upgrade team </a> voor opties.';
+$lang['support_day_remaining'] = 'Overblijvende dag';
 $lang['support_days_remaining'] = 'Resterende dagen';
 $lang['support_documentation'] = 'Documentatie';
-$lang['support_knowledge_base'] = 'Geavanceerde Knowledge Base';
+$lang['support_knowledge_base'] = 'Geavanceerde kennisbasis';
 $lang['support_learn_more'] = 'Kom meer te weten';
-$lang['support_no_open_tickets'] = 'Geen Open Tickets';
+$lang['support_no_open_tickets'] = 'Geen open tickets';
 $lang['support_not_available'] = 'Niet beschikbaar';
 $lang['support_phone'] = 'Telefoon';
-$lang['support_please_open_ticket'] = 'Open een support ticket voor het starten van een chatsessie.';
-$lang['support_realtime_chat'] = 'Real Time Chat Ondersteuning';
-$lang['support_submit_bug_report'] = 'Verzend een Bug Report';
-$lang['support_submit_ticket'] = 'Verzend een support ticket';
-$lang['support_upgrades_and_per_incident'] = 'Om in aanmerking te komen voor steun, dan kunt u de aanschaf van een per-incident ticket of een upgrade van uw abonnement <a href="#" class="support-upgrade-url"> hier </a> .';
+$lang['support_please_open_ticket'] = 'Open een supportticket voordat u een chatsessie start.';
+$lang['support_realtime_chat'] = 'Real-time chatsupport';
+$lang['support_submit_bug_report'] = 'Dien een bugrapport in';
+$lang['support_submit_ticket'] = 'Dien een supportticket in';
+$lang['support_upgrades_and_per_incident'] = 'Als u in aanmerking wilt komen voor ondersteuning, koopt u een per incident-ticket of verbetert u uw abonnementsniveau <a href="#" class="support-upgrade-url"> hier </a> .';
 $lang['support_upgrade'] = 'Upgrade';
-$lang['support_upgrade_window'] = 'upgrade in aanmerking te komen';
+$lang['support_upgrade_window'] = 'Upgrade Geschiktheid';
 $lang['support_web'] = 'Web';
