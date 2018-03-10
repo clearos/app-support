42a544aed2e8652a1e00f01ddde009463205136f
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:00:43 +0000

    Add new files and directories, commit changes, push changes

--- language/el/support_lang.php
+++ language/el/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Επιλογές και τους πόρους για την υποστήριξη του συστήματός σας. Αν θα θέλατε να συζητήσουμε τις απαιτήσεις σας, παρακαλούμε <a href="#" class="support-contact"> επικοινωνήστε μαζί μας </a> .';
+$lang['support_app_description'] = 'Επιλογές και πόροι για την υποστήριξη του συστήματός σας. Εάν θέλετε να συζητήσετε τις απαιτήσεις σας, παρακαλώ <a href="#" class="support-contact"> επικοινωνήστε μαζί μας </a> .';
 $lang['support_app_name'] = 'Υποστήριξη';
-$lang['support_chat_not_available'] = 'Υποστήριξη μέσω συνομιλίας δεν είναι διαθέσιμη αυτή τη στιγμή.';
-$lang['support_community_forums'] = 'Κοινότητα Φόρουμ';
-$lang['support_contact_upgrades'] = 'Επικοινωνήστε μαζί μας <a href="#" class="support-contact"> αναβάθμιση της ομάδας </a> για τις επιλογές.';
-$lang['support_day_remaining'] = 'ημέρα Παραμένοντας';
+$lang['support_chat_not_available'] = 'Η υποστήριξη μέσω συζήτησης δεν είναι διαθέσιμη αυτή τη στιγμή.';
+$lang['support_community_forums'] = 'Φόρουμ της κοινότητας';
+$lang['support_contact_upgrades'] = 'Επικοινωνήστε με μας <a href="#" class="support-contact"> ομάδα αναβάθμισης </a> για επιλογές.';
+$lang['support_day_remaining'] = 'Ημέρα που μένει';
 $lang['support_days_remaining'] = 'Ημέρες που απομένουν';
 $lang['support_documentation'] = 'Απόδειξη με έγγραφα';
-$lang['support_knowledge_base'] = 'Σύνθετη Γνωσιακής Βάσης';
+$lang['support_knowledge_base'] = 'Προηγμένη βάση γνώσεων';
 $lang['support_learn_more'] = 'Μάθε περισσότερα';
-$lang['support_no_open_tickets'] = 'Δεν Open Εισιτήρια';
+$lang['support_no_open_tickets'] = 'Δεν υπάρχουν ανοιχτά εισιτήρια';
 $lang['support_not_available'] = 'Μη διαθέσιμος';
 $lang['support_phone'] = 'Τηλέφωνο';
-$lang['support_please_open_ticket'] = 'Παρακαλούμε ανοίξτε ένα εισιτήριο υποστήριξης πριν από την έναρξη μιας συνομιλίας.';
-$lang['support_realtime_chat'] = 'Chat υποστήριξη σε πραγματικό χρόνο';
-$lang['support_submit_bug_report'] = 'Υποβολή μια αναφορά σφαλμάτων';
-$lang['support_submit_ticket'] = 'Υποβάλετε μια αίτηση υποστήριξης';
-$lang['support_upgrades_and_per_incident'] = 'Για να είναι επιλέξιμα για στήριξη, παρακαλούμε να αγοράσουν ένα εισιτήριο ανά περιστατικό ή να αναβαθμίσουν το επίπεδο της συνδρομής σας <a href="#" class="support-upgrade-url"> εδώ </a> .';
+$lang['support_please_open_ticket'] = 'Ανοίξτε ένα εισιτήριο υποστήριξης πριν ξεκινήσετε μια συνομιλία.';
+$lang['support_realtime_chat'] = 'Υποστήριξη συνομιλίας σε πραγματικό χρόνο';
+$lang['support_submit_bug_report'] = 'Υποβάλετε μια αναφορά σφαλμάτων';
+$lang['support_submit_ticket'] = 'Υποβολή ενός εισιτηρίου υποστήριξης';
+$lang['support_upgrades_and_per_incident'] = 'Για να δικαιούστε υποστήριξη, αγοράστε εισιτήριο ανά περιστατικό ή αναβαθμίστε το επίπεδο συνδρομής σας <a href="#" class="support-upgrade-url"> εδώ </a> .';
 $lang['support_upgrade'] = 'Αναβαθμίζω';
-$lang['support_upgrade_window'] = 'Αναβάθμιση Επιλεξιμότητα';
+$lang['support_upgrade_window'] = 'Αναβάθμιση επιλεξιμότητας';
 $lang['support_web'] = 'Ιστός';
