77dda4f4457b577a84b21ba23346626cd0d6baa3
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:08:44 +0000

    Add new files and directories, commit changes, push changes

--- language/ka/support_lang.php
+++ language/ka/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'პარამეტრები და რესურსები მხარდამჭერი სისტემა. თუ გსურთ განიხილოს თქვენი მოთხოვნები, გთხოვთ <a href="#" class="support-contact"> დაგვიკავშირდით </a> .';
+$lang['support_app_description'] = 'პარამეტრები და რესურსები თქვენი სისტემის მხარდაჭერისთვის. თუ გსურთ თქვენი მოთხოვნების განხილვა, გთხოვთ <a href="#" class="support-contact"> დაგვიკავშირდით </a> .';
 $lang['support_app_name'] = 'მხარდაჭერა';
-$lang['support_chat_not_available'] = 'მხარდაჭერა მეშვეობით სტატისტიკა მიუწვდომელია ამ დროს.';
+$lang['support_chat_not_available'] = 'ამ დროისთვის მხარდაჭერის სტატისტიკა არ არის ხელმისაწვდომი.';
 $lang['support_community_forums'] = 'ფორუმი';
-$lang['support_contact_upgrades'] = 'კონტაქტი ჩვენი <a href="#" class="support-contact"> განახლება გუნდი </a> პარამეტრები.';
-$lang['support_day_remaining'] = 'დღის დარჩენილი';
-$lang['support_days_remaining'] = 'დღის დარჩენილი';
+$lang['support_contact_upgrades'] = 'დაგვიკავშირდით <a href="#" class="support-contact"> განახლების გუნდი </a> პარამეტრები.';
+$lang['support_day_remaining'] = 'დარჩენილი დღე';
+$lang['support_days_remaining'] = 'დარჩენილი დღეები';
 $lang['support_documentation'] = 'დოკუმენტაცია';
-$lang['support_knowledge_base'] = 'Advanced ბაზა';
+$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
 $lang['support_learn_more'] = 'გაიგე მეტი';
-$lang['support_no_open_tickets'] = 'არარის Open ბილეთები';
+$lang['support_no_open_tickets'] = 'არა ღია ბილეთები';
 $lang['support_not_available'] = 'მიუწვდომელია';
 $lang['support_phone'] = 'ტელეფონი';
-$lang['support_please_open_ticket'] = 'გთხოვთ გახსენით დახმარების ბარათი დაწყებამდე სტატისტიკა სხდომაზე.';
-$lang['support_realtime_chat'] = 'რეალური დრო Chat Support';
-$lang['support_submit_bug_report'] = 'წარუდგინოს Bug Report';
-$lang['support_submit_ticket'] = 'გაგზავნოთ დახმარების ბილეთის';
-$lang['support_upgrades_and_per_incident'] = 'იმისათვის რომ უფლება მხარდაჭერა, გთხოვთ შეძენა თითო ინციდენტი ბილეთი ან განახლება თქვენი სააბონენტო დონეზე <a href="#" class="support-upgrade-url"> აქ </a> .';
+$lang['support_please_open_ticket'] = 'გთხოვთ, გაიაროთ მხარდაჭერის ბილეთი ჩეთი სხდომის დაწყებამდე.';
+$lang['support_realtime_chat'] = 'რეალური დრო ჩატი მხარდაჭერა';
+$lang['support_submit_bug_report'] = 'წარმოადგინეთ Bug Report';
+$lang['support_submit_ticket'] = 'წარუდგინეთ დახმარების ბილეთი';
+$lang['support_upgrades_and_per_incident'] = 'მხარდასაჭერად უნდა იყოთ, გთხოვთ, მიიღოთ თითო ინციდენტი ბილეთი ან განაახლეთ გამოწერის დონე <a href="#" class="support-upgrade-url"> აქ </a> .';
 $lang['support_upgrade'] = 'განახლება';
-$lang['support_upgrade_window'] = 'განახლების უფლება';
+$lang['support_upgrade_window'] = 'გაუმჯობესების უფლება';
 $lang['support_web'] = 'ვებ';
