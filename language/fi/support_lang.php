dc16c9a260a4f5bf5498ba256aecda7b4856caf4
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 09:58:17 +0000

    Add new files and directories, commit changes, push changes

--- language/fi/support_lang.php
+++ language/fi/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Vaihtoehdot ja resursseja tukemaan järjestelmään. Jos haluat keskustella vaatimuksia, ota <a href="#" class="support-contact"> ota meihin yhteyttä </a> .';
+$lang['support_app_description'] = 'Asetukset ja resurssit järjestelmän tukemiseksi. Jos haluat keskustella vaatimuksistasi, ole hyvä <a href="#" class="support-contact"> ota meihin yhteyttä </a> .';
 $lang['support_app_name'] = 'Tuki';
-$lang['support_chat_not_available'] = 'Support chat ei ole tällä kertaa.';
-$lang['support_community_forums'] = 'yhteisön Forums';
-$lang['support_contact_upgrades'] = 'Ota yhteyttä <a href="#" class="support-contact"> päivitys joukkue </a> vaihtoehdot.';
-$lang['support_day_remaining'] = 'päivä jäljellä';
+$lang['support_chat_not_available'] = 'Chat-tuki ei ole tällä hetkellä käytettävissä.';
+$lang['support_community_forums'] = 'Yhteisöfoorumit';
+$lang['support_contact_upgrades'] = 'Ota yhteyttä <a href="#" class="support-contact"> päivitysryhmä </a> vaihtoehdoista.';
+$lang['support_day_remaining'] = 'Päivä on jäljellä';
 $lang['support_days_remaining'] = 'Päiviä jäljellä';
 $lang['support_documentation'] = 'Dokumentointi';
-$lang['support_knowledge_base'] = 'Advanced Knowledge Base';
-$lang['support_learn_more'] = 'lisätietoja';
-$lang['support_no_open_tickets'] = 'Ei Avoin Liput';
+$lang['support_knowledge_base'] = 'Kehittynyt tietämyskanta';
+$lang['support_learn_more'] = 'Lisätietoja';
+$lang['support_no_open_tickets'] = 'Ei avaa liput';
 $lang['support_not_available'] = 'Ei saatavilla';
 $lang['support_phone'] = 'Puhelin';
-$lang['support_please_open_ticket'] = 'Avaa tukipyynnön ennen aloittamista chat-istunnon.';
-$lang['support_realtime_chat'] = 'Real Time Chat Support';
-$lang['support_submit_bug_report'] = 'Jätä Bug Report';
-$lang['support_submit_ticket'] = 'Jätä Support Ticket';
-$lang['support_upgrades_and_per_incident'] = 'Voidakseen saada tukea, hanki per-tapaus lippu tai päivittää tilaustason <a href="#" class="support-upgrade-url"> tässä </a> .';
+$lang['support_please_open_ticket'] = 'Avaa tukilippu ennen chat-istunnon aloittamista.';
+$lang['support_realtime_chat'] = 'Reaaliaikaista chat-tukea';
+$lang['support_submit_bug_report'] = 'Lähetä virheilmoitus';
+$lang['support_submit_ticket'] = 'Lähetä tukilippu';
+$lang['support_upgrades_and_per_incident'] = 'Jotta voit saada tukea, ota tilapäinen lippu tai päivitä tilaustasosi <a href="#" class="support-upgrade-url"> tässä </a> .';
 $lang['support_upgrade'] = 'parantaa';
-$lang['support_upgrade_window'] = 'Päivitys Kelpoisuus';
+$lang['support_upgrade_window'] = 'Päivitä tukikelpoisuus';
 $lang['support_web'] = 'verkko';
