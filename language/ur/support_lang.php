883423750f170115b970c0c96030511767969b76
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:36:09 +0000

    Add new files and directories, commit changes, push changes

--- language/ur/support_lang.php
+++ language/ur/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'آپ کے نظام کی حمایت کے لئے اختیارات اور وسائل. آپ براہ مہربانی، آپ کی ضروریات پر بات چیت کرنا چاہتے ہیں تو <a href="#" class="support-contact"> ہم سے رابطہ کریں </a> .';
+$lang['support_app_description'] = 'آپ کے نظام کی حمایت کے لئے اختیارات اور وسائل. اگر آپ اپنی ضروریات پر تبادلہ خیال کرنا چاہتے ہیں تو، براہ مہربانی <a href="#" class="support-contact"> ہم سے رابطہ کریں </a> .';
 $lang['support_app_name'] = 'سپورٹ';
-$lang['support_chat_not_available'] = 'چیٹ کے ذریعے سپورٹ اس وقت دستیاب نہیں ہے.';
-$lang['support_community_forums'] = 'فورم';
-$lang['support_contact_upgrades'] = 'رابطہ کریں ہم سے <a href="#" class="support-contact"> اپ گریڈ کی ٹیم </a> اختیارات کے لئے.';
-$lang['support_day_remaining'] = 'باقی کے دن';
+$lang['support_chat_not_available'] = 'چیٹ کے ذریعے معاونت اس وقت دستیاب نہیں ہے.';
+$lang['support_community_forums'] = 'کمیونٹی فورمز';
+$lang['support_contact_upgrades'] = 'ہم سے رابطہ کریں <a href="#" class="support-contact"> اپ گریڈ ٹیم </a> اختیارات کے لئے.';
+$lang['support_day_remaining'] = 'دن باقی';
 $lang['support_days_remaining'] = 'باقی دنوں';
 $lang['support_documentation'] = 'دستاویزی';
-$lang['support_knowledge_base'] = 'اعلی درجے سویدی';
+$lang['support_knowledge_base'] = 'اعلی درجے کی علم کی بنیاد';
 $lang['support_learn_more'] = 'اورجانیے';
-$lang['support_no_open_tickets'] = 'کوئی کھلی ٹکٹ';
+$lang['support_no_open_tickets'] = 'کوئی کھلی ٹکٹ نہیں';
 $lang['support_not_available'] = 'دستیاب نہیں ہے';
 $lang['support_phone'] = 'فون';
-$lang['support_please_open_ticket'] = 'ایک بات چیت سیشن شروع کرنے سے پہلے ایک سپورٹ ٹکٹ کھولنے کریں.';
-$lang['support_realtime_chat'] = 'ریئل ٹائم چیٹ سپورٹ';
-$lang['support_submit_bug_report'] = 'بگ کی رپورٹ جمع کرائیں';
-$lang['support_submit_ticket'] = 'ایک سپورٹ ٹکٹ جمع کرائیں';
-$lang['support_upgrades_and_per_incident'] = 'کی حمایت کے لئے اہل ہونے کے لئے، ایک فی واقعے کے ٹکٹ کی خریداری یا آپ کی رکنیت کی سطح اپ گریڈ کریں <a href="#" class="support-upgrade-url"> یہاں </a> .';
+$lang['support_please_open_ticket'] = 'چیٹ سیشن شروع کرنے سے قبل براہ کرم سپورٹ ٹکٹ کھولیں.';
+$lang['support_realtime_chat'] = 'اصلی وقت چیٹ سپورٹ';
+$lang['support_submit_bug_report'] = 'بگ رپورٹ جمع کروائیں';
+$lang['support_submit_ticket'] = 'سپورٹ ٹکٹ جمع کروائیں';
+$lang['support_upgrades_and_per_incident'] = 'معاونت کے اہل ہونے کے لئے، براہ کرم ایک واقعہ کے ٹکٹ خریدنے یا اپنی رکنیت کی سطح کو اپ گریڈ کریں <a href="#" class="support-upgrade-url"> یہاں </a> .';
 $lang['support_upgrade'] = 'اپ گریڈ';
-$lang['support_upgrade_window'] = 'اہلیت کو اپ ڈیٹ کریں';
+$lang['support_upgrade_window'] = 'اہلیت کو اپ گریڈ کریں';
 $lang['support_web'] = 'ویب';
