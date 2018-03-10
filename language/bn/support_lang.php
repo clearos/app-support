1795b978e40d9f450dd609238bf46ae47ec94c71
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 09:50:34 +0000

    Add new files and directories, commit changes, push changes

--- language/bn/support_lang.php
+++ language/bn/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'বিকল্প এবং আপনার সিস্টেম সমর্থনের জন্য সম্পদ. আপনি আপনার প্রয়োজনীয়তা, দয়া করে আলোচনা করতে চাই তাহলে <a href="#" class="support-contact"> আমাদের সাথে যোগাযোগ করুন </a> .';
+$lang['support_app_description'] = 'আপনার সিস্টেম সমর্থন করার জন্য বিকল্প এবং সম্পদ। আপনি যদি আপনার প্রয়োজনীয়তা নিয়ে আলোচনা করতে চান, দয়া করে <a href="#" class="support-contact"> আমাদের সাথে যোগাযোগ করুন </a> ।';
 $lang['support_app_name'] = 'সমর্থন';
-$lang['support_chat_not_available'] = 'চ্যাটের মাধ্যমে সাপোর্ট এই সময়ে অনুপলব্ধ.';
+$lang['support_chat_not_available'] = 'চ্যাটের মাধ্যমে সমর্থন এই সময়ে অনুপলব্ধ।';
 $lang['support_community_forums'] = 'কমিউনিটি ফোরাম';
-$lang['support_contact_upgrades'] = 'যোগাযোগ আমাদের <a href="#" class="support-contact"> আপগ্রেড দল </a> বিকল্পের জন্য.';
-$lang['support_day_remaining'] = 'দিন অবশিষ্ট';
+$lang['support_contact_upgrades'] = 'আমাদের সাথে যোগাযোগ করুন <a href="#" class="support-contact"> আপগ্রেড দল </a> বিকল্পগুলির জন্য';
+$lang['support_day_remaining'] = 'দিন বাকি';
 $lang['support_days_remaining'] = 'অবশিষ্ট দিন';
-$lang['support_documentation'] = 'ডকুমেন্টেশন';
-$lang['support_knowledge_base'] = 'উন্নত নলেজ বেস';
+$lang['support_documentation'] = 'নথিপত্র';
+$lang['support_knowledge_base'] = 'উন্নত জ্ঞান বজায়';
 $lang['support_learn_more'] = 'আরও জানুন';
-$lang['support_no_open_tickets'] = 'কোন ওপেন টিকেট';
+$lang['support_no_open_tickets'] = 'কোনও খোলা টিকিট নেই';
 $lang['support_not_available'] = 'পাওয়া যায় না';
 $lang['support_phone'] = 'ফোন';
-$lang['support_please_open_ticket'] = 'অনুগ্রহ করে একটি চ্যাট সেশন শুরু করার আগে একটি সমর্থন টিকেট খুলুন.';
-$lang['support_realtime_chat'] = 'রিয়েল টাইম চ্যাট সাপোর্ট';
+$lang['support_please_open_ticket'] = 'একটি চ্যাট সেশন শুরু করার আগে একটি সমর্থন টিকেট খুলুন।';
+$lang['support_realtime_chat'] = 'রিয়েল টাইম চ্যাট সমর্থন';
 $lang['support_submit_bug_report'] = 'একটি বাগ রিপোর্ট জমা দিন';
-$lang['support_submit_ticket'] = 'জমা সমর্থন টিকেট';
-$lang['support_upgrades_and_per_incident'] = 'সমর্থনের জন্য যোগ্য হতে, একটি প্রতি-ঘটনা টিকেট ক্রয় করা যাবে অথবা সাবস্ক্রিপশন স্তর আপগ্রেড করুন <a href="#" class="support-upgrade-url"> এখানে </a> .';
+$lang['support_submit_ticket'] = 'একটি সমর্থন টিকিট জমা দিন';
+$lang['support_upgrades_and_per_incident'] = 'সমর্থনের জন্য যোগ্য হওয়ার জন্য, দয়া করে প্রতি-ইভেন্ট টিকিটটি ক্রয় করুন বা আপনার সাবস্ক্রিপশন স্তরটি আপগ্রেড করুন <a href="#" class="support-upgrade-url"> এখানে </a> ।';
 $lang['support_upgrade'] = 'আপগ্রেড';
-$lang['support_upgrade_window'] = 'আপগ্রেড যোগ্যতা';
+$lang['support_upgrade_window'] = 'যোগ্যতা আপগ্রেড করুন';
 $lang['support_web'] = 'ওয়েব';
