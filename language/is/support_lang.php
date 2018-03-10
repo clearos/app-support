0358c514fd8c9c1a9c1159c282836e7234a6a7e3
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:06:34 +0000

    Add new files and directories, commit changes, push changes

---
+++ language/is/support_lang.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang['support_app_description'] = 'Valkostir og úrræði til að styðja kerfið þitt. Ef þú vilt ræða kröfur þínar skaltu vinsamlegast <a href="#" class="support-contact"> Hafðu samband við okkur </a> .';
+$lang['support_app_name'] = 'Stuðningur';
+$lang['support_chat_not_available'] = 'Stuðningur í gegnum spjall er ekki tiltæk á þessum tíma.';
+$lang['support_community_forums'] = 'Community Forums';
+$lang['support_contact_upgrades'] = 'Hafðu samband við okkar <a href="#" class="support-contact"> uppfærsla lið </a> fyrir valkosti.';
+$lang['support_day_remaining'] = 'Dagur eftir';
+$lang['support_days_remaining'] = 'Dagar eftir';
+$lang['support_documentation'] = 'Skjalfesting';
+$lang['support_knowledge_base'] = 'Ítarlegri þekkingargrunn';
+$lang['support_learn_more'] = 'Læra meira';
+$lang['support_no_open_tickets'] = 'Engar opnar miðar';
+$lang['support_not_available'] = 'Ekki í boði';
+$lang['support_phone'] = 'Sími';
+$lang['support_please_open_ticket'] = 'Opnaðu stuðnings miða áður en spjallþáttur hefst.';
+$lang['support_realtime_chat'] = 'Real Time Chat Support';
+$lang['support_submit_bug_report'] = 'Sendu inn villuskýrslu';
+$lang['support_submit_ticket'] = 'Sendu inn stoðarmiða';
+$lang['support_upgrades_and_per_incident'] = 'Til að vera gjaldgengur fyrir stuðning skaltu vinsamlegast kaupa hverja atburðarmiða eða uppfæra áskriftarstig þitt <a href="#" class="support-upgrade-url"> hér </a> .';
+$lang['support_upgrade'] = 'Uppfærsla';
+$lang['support_upgrade_window'] = 'Uppfærsla hæfi';
+$lang['support_web'] = 'vefur';
