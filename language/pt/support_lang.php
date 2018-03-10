7d9d5d6751bdb15db523d2867c2b7df41c2eec2c
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:23:06 +0000

    Add new files and directories, commit changes, push changes

--- language/pt/support_lang.php
+++ language/pt/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'Opções e recursos para apoiar seu sistema. Se você gostaria de discutir suas necessidades, por favor <a href="#" class="support-contact"> Contate-Nos </a> .';
+$lang['support_app_description'] = 'Opções e recursos para suportar seu sistema. Se você gostaria de discutir seus requisitos, por favor <a href="#" class="support-contact"> Contate-Nos </a> .';
 $lang['support_app_name'] = 'Apoio, suporte';
-$lang['support_chat_not_available'] = 'Suporte via chat está indisponível no momento.';
-$lang['support_community_forums'] = 'Fóruns Comunitários';
-$lang['support_contact_upgrades'] = 'entre em contato com o nosso <a href="#" class="support-contact"> equipe de atualização </a> para opções.';
-$lang['support_day_remaining'] = 'dia Permanecendo';
+$lang['support_chat_not_available'] = 'O suporte por chat não está disponível neste momento.';
+$lang['support_community_forums'] = 'Fóruns comunitários';
+$lang['support_contact_upgrades'] = 'Contate nosso <a href="#" class="support-contact"> equipe de atualização </a> para opções.';
+$lang['support_day_remaining'] = 'Dia restante';
 $lang['support_days_remaining'] = 'Dias restantes';
 $lang['support_documentation'] = 'Documentação';
-$lang['support_knowledge_base'] = 'Base de Conhecimento avançado';
+$lang['support_knowledge_base'] = 'Base de conhecimento avançada';
 $lang['support_learn_more'] = 'Saber mais';
-$lang['support_no_open_tickets'] = 'Nenhuns bilhetes Abertas';
+$lang['support_no_open_tickets'] = 'No Open Tickets';
 $lang['support_not_available'] = 'Não disponível';
 $lang['support_phone'] = 'telefone';
-$lang['support_please_open_ticket'] = 'Por favor, abra um ticket de suporte antes de iniciar uma sessão de chat.';
-$lang['support_realtime_chat'] = 'Suporte chat em tempo real';
-$lang['support_submit_bug_report'] = 'Enviar um relatório de erro';
-$lang['support_submit_ticket'] = 'Enviar Ticket Suporte';
-$lang['support_upgrades_and_per_incident'] = 'Para serem elegíveis para apoio, por favor, comprar um bilhete por incidente ou atualizar seu nível de subscrição <a href="#" class="support-upgrade-url"> Aqui </a> .';
+$lang['support_please_open_ticket'] = 'Abra um ticket de suporte antes de iniciar uma sessão de bate-papo.';
+$lang['support_realtime_chat'] = 'Suporte de bate-papo em tempo real';
+$lang['support_submit_bug_report'] = 'Enviar um Relatório de Erro';
+$lang['support_submit_ticket'] = 'Enviar um Bilhete de Suporte';
+$lang['support_upgrades_and_per_incident'] = 'Para ser elegível para suporte, compre um ticket por incidente ou atualize seu nível de assinatura <a href="#" class="support-upgrade-url"> Aqui </a> .';
 $lang['support_upgrade'] = 'Atualizar';
-$lang['support_upgrade_window'] = 'Melhora a Elegibilidade';
+$lang['support_upgrade_window'] = 'Elegibilidade de atualização';
 $lang['support_web'] = 'Rede';
