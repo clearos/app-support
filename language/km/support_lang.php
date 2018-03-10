55b47f1e4ebd1bd3825fe344edb222985b3485d9
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:09:58 +0000

    Add new files and directories, commit changes, push changes

--- language/km/support_lang.php
+++ language/km/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'ជម្រើសនិងធនធានសម្រាប់គាំទ្រដល់ប្រព័ន្ធរបស់អ្នក។ ប្រសិនបើអ្នកចង់ដើម្បីពិភាក្សាអំពីតម្រូវការរបស់អ្នក, សូម <a href="#" class="support-contact"> ទាក់ទង​មក​ពួក​យើង </a> ។';
+$lang['support_app_description'] = 'ជម្រើសនិងធនធានសម្រាប់គាំទ្រប្រព័ន្ធរបស់អ្នក។ ប្រសិនបើអ្នកចង់ពិភាក្សាអំពីតម្រូវការរបស់អ្នកសូម <a href="#" class="support-contact"> ទាក់ទង​មក​ពួក​យើង </a> ។';
 $lang['support_app_name'] = 'ការគាំទ្រ';
-$lang['support_chat_not_available'] = 'គាំទ្រតាមរយៈការជជែកគឺមិនអាចប្រើបាននៅពេលនេះ។';
+$lang['support_chat_not_available'] = 'ការគាំទ្រតាមរយៈការជជែកមិនអាចប្រើបានទេនៅពេលនេះ។';
 $lang['support_community_forums'] = 'វេទិកាសហគមន៍';
-$lang['support_contact_upgrades'] = 'សូមទាក់ទងរបស់យើង <a href="#" class="support-contact"> ក្រុមធ្វើឱ្យប្រសើរឡើង </a> សម្រាប់ជម្រើស។';
+$lang['support_contact_upgrades'] = 'ទាក់ទងមកយើង <a href="#" class="support-contact"> ធ្វើឱ្យប្រសើរឡើងក្រុម </a> សម្រាប់ជម្រើស។';
 $lang['support_day_remaining'] = 'ថ្ងៃដែលនៅសល់';
-$lang['support_days_remaining'] = 'ថ្ងៃដែលនៅសេសសល់';
+$lang['support_days_remaining'] = 'ថ្ងៃដែលនៅសល់';
 $lang['support_documentation'] = 'ឯកសារ';
-$lang['support_knowledge_base'] = 'កម្រិតខ្ពស់មូលដ្ឋានចំណេះដឹង';
+$lang['support_knowledge_base'] = 'មូលដ្ឋានចំណេះដឹងកម្រិតខ្ពស់';
 $lang['support_learn_more'] = 'ស្វែង​យល់​បន្ថែម';
-$lang['support_no_open_tickets'] = 'គ្មានការបើកសំបុត្រ';
+$lang['support_no_open_tickets'] = 'មិនមានសំបុត្របើកចំហ';
 $lang['support_not_available'] = 'មិនអាច';
 $lang['support_phone'] = 'ទូរស័ព្ទ';
-$lang['support_please_open_ticket'] = 'សូមបើកសំបុត្រការគាំទ្រមួយមុនពេលដែលផ្តួចផ្តើមសម័យជជែកកំសាន្តមួយ។';
-$lang['support_realtime_chat'] = 'ពេលវេលាពិតប្រាកដជជែកកំសាន្តជំនួយស្មារតី';
+$lang['support_please_open_ticket'] = 'សូមបើកសំបុត្រគាំទ្រមុនពេលចាប់ផ្ដើមវគ្គជជែក។';
+$lang['support_realtime_chat'] = 'ការគាំទ្រការជជែកកំសាន្តពេលវេលាពិត';
 $lang['support_submit_bug_report'] = 'ដាក់ស្នើរបាយការណ៍កំហុស';
-$lang['support_submit_ticket'] = 'ដាក់ស្នើសំបុត្រការគាំទ្រ';
-$lang['support_upgrades_and_per_incident'] = 'ដើម្បីមានសិទ្ធិទទួលបានការគាំទ្រសូមទិញសំបុត្រក្នុងមួយឧប្បត្តិហេតុឬធ្វើឱ្យប្រសើរកម្រិតជាវរបស់អ្នក <a href="#" class="support-upgrade-url"> នៅទីនេះ </a> ។';
-$lang['support_upgrade'] = 'ធ្វើឱ្យប្រសើរឡើង';
-$lang['support_upgrade_window'] = 'ធ្វើឱ្យប្រសើរឡើងសិទ្ធិទទួល';
+$lang['support_submit_ticket'] = 'ដាក់ស្នើសំបុត្រគាំទ្រ';
+$lang['support_upgrades_and_per_incident'] = 'ដើម្បីមានសិទ្ធិទទួលបានការគាំទ្រសូមទិញសំបុត្រក្នុងមួយ - ហេតុការណ៍ឬធ្វើឱ្យប្រសើរឡើងកម្រិតនៃការជាវរបស់អ្នក <a href="#" class="support-upgrade-url"> នៅទីនេះ </a> ។';
+$lang['support_upgrade'] = 'ធ្វើបច្ចុប្បន្នភាព';
+$lang['support_upgrade_window'] = 'ធ្វើបច្ចុប្បន្នភាពសិទ្ធិ';
 $lang['support_web'] = 'បណ្តាញ';
