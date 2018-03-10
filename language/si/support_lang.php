b1932c6802eb07ea89feb061208657d01214847d
Apache <apache@ip-172-31-43-245.us-west-2.compute.internal>
2018-03-10 10:25:36 +0000

    Add new files and directories, commit changes, push changes

--- language/si/support_lang.php
+++ language/si/support_lang.php
@@ -1,23 +1,23 @@
 <?php
 
-$lang['support_app_description'] = 'ඔබේ පද්ධතිය සඳහා සහාය විකල්ප සහ සම්පත්. ඔබ, ඔබේ අවශ්යතා ගැන සාකච්ඡා කිරීමට කැමති නම් කරුණාකර <a href="#" class="support-contact"> අප අමතන්න </a> .';
+$lang['support_app_description'] = 'ඔබේ පද්ධතියට සහාය දීම සඳහා විකල්ප සහ සම්පත්. කරුණාකර ඔබගේ අවශ්යතා සාකච්ඡා කිරීමට කැමති නම්, කරුණාකර <a href="#" class="support-contact"> අප අමතන්න </a> .';
 $lang['support_app_name'] = 'සහාය';
-$lang['support_chat_not_available'] = 'චැට් හරහා සහාය මේ කාලය වන විට අත්හිටුවා ඇත.';
+$lang['support_chat_not_available'] = 'මෙම වේලාව තුළ චැට් හරහා සහාය දීමක් නොමැත.';
 $lang['support_community_forums'] = 'ප්රජා සංසද';
-$lang['support_contact_upgrades'] = 'අපගේ අමතන්න <a href="#" class="support-contact"> ප්රගමනය කණ්ඩායම </a> විකල්ප සඳහා.';
-$lang['support_day_remaining'] = 'දින ඉතිරි';
-$lang['support_days_remaining'] = 'ඉතිරි දින';
-$lang['support_documentation'] = 'ලේඛගතකිරීම';
-$lang['support_knowledge_base'] = 'උසස් දැනුම මූලික';
+$lang['support_contact_upgrades'] = 'අප අමතන්න <a href="#" class="support-contact"> උත්ශ්රේණි කිරීම කණ්ඩායම </a> විකල්ප සඳහා.';
+$lang['support_day_remaining'] = 'දිනයේ ඉතිරි වේ';
+$lang['support_days_remaining'] = 'ඉතිරි දවස්';
+$lang['support_documentation'] = 'ලේඛනගත කිරීම';
+$lang['support_knowledge_base'] = 'උසස් දැනුම් පදනමක්';
 $lang['support_learn_more'] = 'වැඩිදුර ඉගෙන ගන්න';
-$lang['support_no_open_tickets'] = 'කිසිදු විවෘත ප්රවේශ පත්';
+$lang['support_no_open_tickets'] = 'විවෘත ටිකට් පත් නොමැත';
 $lang['support_not_available'] = 'ලද නොහැක';
 $lang['support_phone'] = 'දුරකථන';
-$lang['support_please_open_ticket'] = 'කතා බහ සැසිය ආරම්භ කිරීම පෙර සහයෝගය ටිකට් විවෘත කරන්න.';
-$lang['support_realtime_chat'] = 'තත්කාලීන චැට් සහාය';
-$lang['support_submit_bug_report'] = 'දෝෂ වාර්තාවක් ඉදිරිපත්';
-$lang['support_submit_ticket'] = 'එය සනාථ ටිකට් ඉදිරිපත්';
-$lang['support_upgrades_and_per_incident'] = 'සහාය සඳහා සුදුසුකම් ලබන්නේ එක්-සිද්ධිය ටිකට් මිලදී ගැනීමට හෝ ඔබේ ග්රාහකත්වය මට්ටමේ උත්ශ්රේණි <a href="#" class="support-upgrade-url"> මෙහි </a> .';
+$lang['support_please_open_ticket'] = 'කරුණාකර චැට් සැසිය ආරම්භ කිරීමට කරුණාකර උපකාරක ටිකට්පතක් විවෘත කරන්න.';
+$lang['support_realtime_chat'] = 'තථ්ය කාලීන චැට් සහාය';
+$lang['support_submit_bug_report'] = 'බග් වාර්තාවක් ඉදිරිපත් කරන්න';
+$lang['support_submit_ticket'] = 'උපකාරක ටිකට්පතක් ඉදිරිපත් කරන්න';
+$lang['support_upgrades_and_per_incident'] = 'උපකාර සඳහා සුදුසුකම් ලැබීමට කරුණාකර එක් සිද්ධි ප්රවේශ පත්රයක් මිලදී ගැනීම හෝ ඔබගේ දායකත්ව මට්ටම ඉහළ නැංවීම <a href="#" class="support-upgrade-url"> මෙහි </a> .';
 $lang['support_upgrade'] = 'උත්ශ්රේණි කිරීම';
-$lang['support_upgrade_window'] = 'සුදුසුකම් ප්රගමනය';
+$lang['support_upgrade_window'] = 'සුදුසුකම් වැඩිදියුණු කිරීම';
 $lang['support_web'] = 'වෙබ්';
